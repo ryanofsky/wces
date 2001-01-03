774261ca63e983ad4b33972d2153289d4fa3f210
Russell Yanofsky <russ@yanofsky.org>
2001-01-03 19:09:26 +0000

    Added PHP Based Oracle and Unstable Question Set Editor

---
+++ www/wces/administrators/susagegraph.php
@@ -0,0 +1,24 @@
+<%include('server.inc');
+Header("Content-type: image/gif");
+
+if (function_exists('imagecreate'))
+{
+include('class.graph.inc');
+$a = array(array(0,0),array(0+$blank,0+$filled));
+
+phpplot(array(
+    "cubic" => true,    "transparent" => true,
+    "colorset" => array("lred","wcesyellow"),    "title" => false,    "size"=> array(200,200) ));
+    
+phpdata($a);
+phpdraw("piegraph", array(  "scale" => 1.5,  "showlabel" => false,  "piemode" => "explode",  "drawsets" => array(1)));$fname = tempnam($server_tempdir, "graph");
+phpshow($fname,"gif");
+readfile($fname);
+unlink($fname);
+}
+else
+{
+  //readfile("http://160.39.209.146/wces/administrators/susagegraph.php?blank=$blank&filled=$filled");
+  readfile("http://oracle.yahweh.detour.net/wces/administrators/susagegraph.php?blank=$blank&filled=$filled");
+};
+%>
