d109733ab26026d5797e82f47cb88d6e92bf56ea
Russell Yanofsky <russ@yanofsky.org>
2001-01-11 04:11:54 +0000

    Added PHP Based Oracle and Unstable Question Set Editor

---
+++ www/oracle/aboutus.php
@@ -0,0 +1,33 @@
+<%
+require_once("oracle.inc");
+oracle_top("SEAS Oracle Homepage - About Us","aboutus.gif");
+%>
+      <center><FONT face="Verdana,Trebuchet MS" size=+1>Oracle 00-01 Team</FONT></center>
+      <P align=center><FONT face="Verdana,Trebuchet MS" size=-1><B><I>Project 
+      Manager</I></B> <BR>James Zheng<BR></FONT></P><BR>
+      <P align=center><FONT face = "Verdana, Trebuchet MS" size = -1><B><I>Developers</I></B>
+      <BR>Deborah Leung: <I>Web Master</I><BR></FONT></P><BR>
+      <P align=center><FONT face="Verdana,Trebuchet MS" size=-1><B><I>Research 
+      Staff</I></B> <BR>Lawrence Lee<BR>Ronbir Chowdhary
+      <BR></FONT></P><BR>
+      <P align=center><FONT face="Verdana,Trebuchet MS" size=-1><B><I>Faculty 
+      Advisor</I></B> <BR>Dean Morton Friedman <BR>Dean Jack McGourty 
+      </FONT></P>
+      
+      <HR>
+      <P align = center><FONT face="Verdana,Trebuchet MS" size=-1>Oracle 99-00 Team</FONT>
+      <P align=center><FONT face="Verdana,Trebuchet MS" size=-1><B><I>Project
+      Manager</I></B> <BR>Boyle M Lee<BR></FONT></P><BR>
+      <P align=center><FONT face="Verdana,Trebuchet MS"
+      size=-1><B><I>Developers</I></B> <BR>Alex Haubold: <I>Database Manager,
+      System Admin, Systems Consultant</I> <BR>James Zheng: <I>Web Master,
+      Interface Developer, Javascript</I><BR></FONT></P><BR>
+      <P align=center><FONT face="Verdana,Trebuchet MS" size=-1><B><I>Research
+      Staff</I></B> <BR>Deborah Kar Wai Leung<BR>Hsing Hsing Li<BR>Kamesh
+      Adhikary<BR>Po Yau Chan<BR>Will Lim <BR></FONT></P><BR>
+      <P align=center><FONT face="Verdana,Trebuchet MS" size=-1><B><I>Faculty
+      Advisor</I></B> <BR>Dean Morton Friedman <BR>Dean Jack McGourty
+      </FONT></P>
+<%
+oracle_bottom();
+%>
