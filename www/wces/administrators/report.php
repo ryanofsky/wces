7c970efaf7febf6a6529c5f8769487cdf40355d9
Russell Yanofsky <russ@yanofsky.org>
2001-02-11 23:01:54 +0000

    Restructuring. Moved some files around.

--- www/wces/administrators/report.php
+++ www/wces/administrators/report.php
@@ -1,26 +1,15 @@
 <%
- require_once("widgets.inc");
- require_once("page.inc");
- require_once("login.inc");
- require_once("reporting.inc");
+ require_once("wces/page.inc");
+ require_once("wces/login.inc");
+ require_once("wces/reporting.inc");
  login_protect(login_administrator);
- page_top("Administrative Report","001");
-
-$wiz = new ReportOptions("wizard","wiz",WIDGET_POST);
-$wiz->loadvalues();
-
+ $report = new Report("report","wiz",WIDGET_POST);
+ $report->loadvalues();
+ page_top("Administrative Report","001",$report->hidemenus);
 %>
+
 <form name="wiz" method="post">
-<input type=hidden name=revivewizard value=0>
-<% 
- if (!$wiz->isfinished() || $revivewizard)
-   $wiz->display(false);
- else
- {
-   print("<p><b><a href=\"javascript:document.forms['wiz']['revivewizard'].value = 1; void(document.forms['wiz'].submit())\">Back to wizard</a></b></p>");
-   $wiz->display(true);
-   $wiz->makereport();
- }
-%></form><%
-  page_bottom();
-%>
+<% $report->display(); %>
+</form>
+
+<% page_bottom($report->hidemenus); %>
