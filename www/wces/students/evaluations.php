6c4a66ad813004d6587d8bc23c026302dadec4f5
Russell Yanofsky <russ@yanofsky.org>
2001-02-28 06:20:12 +0000

    Restructuring. Moved some files around.

--- www/wces/students/evaluations.php
+++ www/wces/students/evaluations.php
@@ -1,21 +1,11 @@
 <%
-  require_once("page.inc");
-  require_once("login.inc");  require_once("wces.inc");  login_protect(login_student);  page_top("Evaluation Listing","0100");
+  require_once("wces/page.inc");
+  require_once("wces/login.inc");  require_once("wces/wces.inc");  login_protect(login_student);  page_top("Evaluation Listing","0100");
   
       $db = wces_connect();
   
-  $y = mysql_query("CREATE TEMPORARY TABLE currentclasses (classid INTEGER NOT NULL, PRIMARY KEY(classid))",$db);
-  $y = mysql_query("REPLACE INTO currentclasses (classid) SELECT cl.classid FROM Groupings AS g INNER JOIN Classes as cl ON g.linkid = cl.classid
-    WHERE g.linktype = 'classes' && cl.year = 2000 && cl.semester = 'fall'",$db);
-  $y = mysql_query("REPLACE INTO currentclasses (classid) SELECT cl.classid FROM Groupings AS g INNER JOIN Classes as cl ON g.linkid = cl.courseid
-    WHERE g.linktype = 'courses' && cl.year = 2000 && cl.semester = 'fall'",$db);
-  $y = mysql_query("REPLACE INTO currentclasses (classid) SELECT cl.classid FROM Groupings AS g INNER JOIN Classes AS cl ON g.linkid = cl.professorid
-    WHERE g.linktype = 'professors'  && cl.year = 2000 && cl.semester = 'fall'",$db);
-  $y = mysql_query("REPLACE INTO currentclasses (classid) SELECT cl.classid FROM Groupings AS g INNER JOIN Courses AS c  ON g.linkid = c.subjectid INNER JOIN Classes as cl ON c.courseid = cl.courseid
-    WHERE g.linktype = 'subjects' && cl.year = 2000 && cl.semester = 'fall'",$db);
-  $y = mysql_query("REPLACE INTO currentclasses (classid) SELECT cl.classid FROM Groupings AS g INNER JOIN Courses AS c  ON g.linkid = c.departmentid INNER JOIN Classes as cl ON c.courseid = cl.courseid 
-    WHERE g.linktype = 'departments' && cl.year = 2000 && cl.semester = 'fall'",$db);
-
+  wces_FindClasses($db,"currentclasses");
+  
   $classes = mysql_query(
   "SELECT cl.classid, cl.section, cl.year, cl.semester, c.code, c.name, s.code AS scode 
   FROM currentclasses AS cc
@@ -25,4 +15,6 @@
     $found = true;    extract($class);
       print ("  <li>$scode$code <i>$name</i> (section $section)</LI>\n");  }
   print("</ul>");
+  
+  mysql_query("DROP TABLE currentclasses",$db);
 %>
