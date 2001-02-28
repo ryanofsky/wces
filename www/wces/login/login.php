fcce4328a003911399ed3829f8a6f94782fff7f0
Russell Yanofsky <russ@yanofsky.org>
2001-02-28 06:13:36 +0000

    Restructuring. Moved some files around.

--- www/wces/login/login.php
+++ www/wces/login/login.php
@@ -1,22 +1,25 @@
 <%
-  require_once("login.inc");
-  require_once("page.inc");
-
-  session_start();  
-  if (!$destination) $destination = "login.php";
-  if (!$requestedstatus) $requestedstatus = login_student;
-  if (!$message) $message = "";
-
+  require_once("wces/login.inc");
+  require_once("wces/page.inc");
   page_top("Login Page");
+  
+  if (!$requestedstatus) $requestedstatus=login_student;
 
   if (login_isloggedin())
   {
-    print("You are currently logged in as '" . login_getuni() . "' as a " . login_getstatus() . ". Click <a href=logout.php>here</a> to log out, or fill out the form below to log in with a different ID.");
     if (!login_isvalid($requestedstatus))
-      print('<br>&nbsp;<br>You have logged in with a valid username and password, but this username has not been given the neccessary permissions to access ' . $destination . ' You can attempt to log in with a different username, or contact an administrator if you believe this to be in error.');
-  };    
+    {
+      print("<p><b><font color=red>Access Denied</font></b></p>");
+      print("<p>You have logged in with a valid username and password, but this username has not been given the neccessary permissions to access <a href=\"$destination\">$destination</a>. You can attempt to log in with a different username, or contact an administrator if you believe this to be in error.");
+    }  
+    print("<p><i>You are currently logged in as '" . login_getuni() . "' as a " . login_getstatus() . ". Click <a href=logout.php>here</a> to log out, or fill in the form below to log in with a different ID.</i></p>");
+  }   
+  else if ($uni)
+    print("<p><b><font color=red>Invalid User Name or Password. Please Try Again</font></b></p>");
+  else
+    print("<p><b>Enter your CUNIX Username and Password below to log in:</b></p>");
 %>
-<form name="login" method="post" action="<%=$server_wcespath%>login/bounce.php"><%=$message%>
+<form name="login" method="post" action="<%=$server_wcespath%>login/bounce.php">
 <table>
 <tr><td>Username:</td><td><input name="uni" type="text" value="<%=htmlspecialchars($uni)%>" size="20"></td></tr>
 <tr><td>Password:</td><td><input name="password" type="password" value="" size="20"></td></tr>
@@ -25,6 +28,18 @@
 <input type="hidden" name=destination value="<%=htmlspecialchars($destination)%>">
 <input type="hidden" name=requestedstatus value="<%=htmlspecialchars($requestedstatus)%>">
 </form>
-<%
+
+<% if (!server_secure()) { %>
+
+<h3>Security Alert</h3>
+
+<p>The information sent in the form above is not encrypted, and could potentially be intercepted
+by someone monitoring http traffic on your network. <a href="http://www.columbia.edu/acis/security/">AcIS</a>recommends that you use encryption whenever possible. To start an encrypted session, click the link below:</p><%$params="destination=" . urlencode($destination) . "&requestedstatus=" .urlencode($requestedstatus) . "&message=" . urlencode($message) . "&uni=" . urlencode($uni); %>
+
+<ul>
+  <li><a href="https://oracle.seas.columbia.edu/wces/login/login.php?<%=$params%>">Secure Server</a></li>
+</ul>
+
+<% }
   page_bottom();
 %>
