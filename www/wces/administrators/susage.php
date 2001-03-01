224f7fd6cef147a04436c0a163e7d54ec0310a19
Russell Yanofsky <russ@yanofsky.org>
2001-03-01 04:13:14 +0000

    Fixes to the database and import code

--- www/wces/administrators/susage.php
+++ www/wces/administrators/susage.php
@@ -9,6 +9,8 @@ $db = wces_connect();
 
 wces_FindClasses($db,"currentclasses");
 
+$questionperiodid = wces_GetQuestionPeriod($db);
+
 mysql_query("CREATE TEMPORARY TABLE surveyclasses (courseid INTEGER NOT NULL, classid INTEGER NOT NULL, section CHAR(3) NOT NULL, scode CHAR(4), code CHAR(5), name TINYTEXT, pname TINYTEXT, professorid INTEGER, students INTEGER, responses INTEGER, PRIMARY KEY(classid))",$db);
 mysql_query("REPLACE INTO surveyclasses (courseid, classid, section, scode, code, name, pname, professorid, students, responses)
 SELECT c.courseid, cc.classid, cl.section, s.code, c.code, c.name, p.name, p.professorid, IFNULL(cl.students,0), IFNULL(MAX(a.responses),0)
@@ -17,7 +19,7 @@ LEFT JOIN Classes AS cl USING (classid)
 LEFT JOIN Courses AS c USING (courseid)
 LEFT JOIN Subjects AS s USING (subjectid)
 LEFT JOIN Professors AS p ON (cl.professorid = p.professorid)
-LEFT JOIN AnswerSets AS a ON (cl.classid = a.classid AND a.questionperiodid = 5)
+LEFT JOIN AnswerSets AS a ON (cl.classid = a.classid AND a.questionperiodid = $questionperiodid)
 GROUP BY cc.classid",$db);
 
 $y = mysql_query("SELECT SUM(students) as students, SUM(responses) as responses FROM surveyclasses",$db);
@@ -41,6 +43,7 @@ $classes = mysql_query("SELECT * FROM surveyclasses ORDER BY (students - respons
 print("<ul>\n");
 while ($class = mysql_fetch_array($classes))
 {
+  $students = $responses = $classid = $professorid = 0; $scode = $code = $section = $name = $pname = "Unknown";
   extract($class);
   $numbers = $students == 0 ? "$responses surveys completed" : (($students - $responses) . " / $students surveys left");
   print ("  <li>$numbers, <a href=\"${server_wcespath}students/classinfo.php?classid=$classid\">$scode$code$section <i>$name</i></a> - Professor <a href=\"${server_wcespath}students/profinfo.php?professorid=$professorid\">$pname</a></li>\n");
