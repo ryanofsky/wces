7ec1a7cee07e8c253555d8561d0fa3c9c703e94a
Russell Yanofsky <russ@yanofsky.org>
2001-04-02 20:29:36 +0000

    Changed professor question editor and added search to oracle

--- www/wces/administrators/pusage.php
+++ www/wces/administrators/pusage.php
@@ -5,24 +5,61 @@ require_once("wces/page.inc");
 login_protect(login_administrator);
 page_top("Usage Data");
 
+$db_debug = true;
+
 $db = wces_connect();
 wces_Findclasses($db,"currentclasses");
 
-$y = mysql_query("CREATE TEMPORARY TABLE customprofessors (professorid INTEGER NOT NULL, dummy INTEGER, PRIMARY KEY (professorid))",$db);
-$y = mysql_query("REPLACE INTO customprofessors (professorid, dummy) SELECT p.professorid, (1) FROM groupings as g INNER JOIN professors as p ON g.linkid = p.professorid 
-  INNER JOIN questionsets AS q ON g.questionsetid = q.questionsetid WHERE g.linktype = 'professors' AND q.type = 'private'",$db);
-$y = mysql_query("REPLACE INTO customprofessors (professorid, dummy) SELECT cl.professorid, (1) FROM groupings as g INNER JOIN classes as cl ON g.linkid = cl.classid
-  INNER JOIN questionsets as q ON g.questionsetid = q.questionsetid WHERE g.linktype = 'classes' AND q.type = 'private'",$db);
-$y = mysql_query("CREATE TEMPORARY TABLE currentprofessors (professorid INTEGER NOT NULL, hasloggedin INTEGER, hascustom INTEGER, students INTEGER, PRIMARY KEY (professorid))",$db);
-$y = mysql_query("REPLACE INTO currentprofessors (professorid, hasloggedin, hascustom, students) SELECT p.professorid, IF(p.userid IS NULL,NULL,1), cmp.dummy, SUM(cl.students) as students FROM currentclasses
-  INNER JOIN classes AS cl ON currentclasses.classid = cl.classid LEFT JOIN professors AS p ON cl.professorid = p.professorid
-  LEFT JOIN customprofessors AS cmp ON p.professorid = cmp.professorid
-  GROUP BY p.professorid",$db);
-$y = mysql_query("DROP TABLE customprofessors",$db);
+$y = db_exec("
+CREATE TEMPORARY TABLE customprofessors
+(
+  professorid INTEGER NOT NULL,
+  PRIMARY KEY (professorid))
+",$db,__FILE__,__LINE__);
 
-$notloggedin = mysql_query("SELECT p.name, cp.professorid, cp.students FROM currentprofessors as cp INNER JOIN professors as p USING (professorid) WHERE cp.hasloggedin IS NULL ORDER BY cp.students DESC",$db);
-$nocustoms   = mysql_query("SELECT p.name, cp.professorid, cp.students FROM currentprofessors as cp INNER JOIN professors as p USING (professorid) WHERE cp.hasloggedin IS NOT NULL AND cp.hascustom IS NULL ORDER BY cp.students DESC",$db);
-$hascustoms  = mysql_query("SELECT p.name, cp.professorid, cp.students FROM currentprofessors as cp INNER JOIN professors as p USING (professorid) WHERE cp.hasloggedin IS NOT NULL AND cp.hascustom IS NOT NULL ORDER BY cp.students DESC",$db);
+$y = db_exec("
+
+REPLACE INTO customprofessors (professorid)
+SELECT p.professorid
+FROM groupings as g
+INNER JOIN professors as p ON g.linkid = p.professorid 
+INNER JOIN questionsets AS q ON g.questionsetid = q.questionsetid
+WHERE g.linktype = 'professors' AND q.type = 'private'
+",$db,__FILE__,__LINE__);
+
+$y = db_exec("
+REPLACE INTO customprofessors (professorid)
+SELECT cl.professorid
+FROM groupings as g INNER JOIN classes as cl ON g.linkid = cl.classid
+INNER JOIN questionsets as q ON g.questionsetid = q.questionsetid
+WHERE g.linktype = 'classes' AND q.type = 'private'
+",$db,__FILE__,__LINE__);
+
+$y = db_exec("
+CREATE TEMPORARY TABLE currentprofessors
+(
+  professorid INTEGER NOT NULL,
+  hasloggedin INTEGER,
+  hascustom INTEGER,
+  students INTEGER,
+  PRIMARY KEY (professorid))
+",$db,__FILE__,__LINE__);
+
+$y = db_exec("
+REPLACE INTO currentprofessors (professorid, hasloggedin, hascustom, students)
+SELECT p.professorid, IF(u.lastlogin > '2001-02-01',1,0), IF(cmp.professorid IS NULL,0,1), SUM(cl.students) as students
+FROM currentclasses
+INNER JOIN classes AS cl ON currentclasses.classid = cl.classid
+INNER JOIN professors AS p ON cl.professorid = p.professorid
+LEFT JOIN users AS u ON p.userid = u.userid
+LEFT JOIN customprofessors AS cmp ON p.professorid = cmp.professorid
+GROUP BY p.professorid",$db,__FILE__,__LINE__);
+
+$y = db_exec("DROP TABLE customprofessors",$db,__FILE__,__LINE__);
+
+$notloggedin = db_exec("SELECT p.name, cp.professorid, cp.students FROM currentprofessors as cp INNER JOIN professors as p USING (professorid) WHERE NOT cp.hasloggedin ORDER BY cp.students DESC",$db,__FILE__,__LINE__);
+$nocustoms   = db_exec("SELECT p.name, cp.professorid, cp.students FROM currentprofessors as cp INNER JOIN professors as p USING (professorid) WHERE cp.hasloggedin AND NOT cp.hascustom ORDER BY cp.students DESC",$db,__FILE__,__LINE__);
+$hascustoms  = db_exec("SELECT p.name, cp.professorid, cp.students FROM currentprofessors as cp INNER JOIN professors as p USING (professorid) WHERE cp.hasloggedin AND cp.hascustom ORDER BY cp.students DESC",$db,__FILE__,__LINE__);
 
 $notloggedincount = mysql_num_rows($notloggedin);
 $hascustomscount = mysql_num_rows($hascustoms);
@@ -50,7 +87,7 @@ print("</ul>");
 <h3>Aggregate Professor Usage</h3>
 
 Number of professors: <b><%=$notloggedincount+$hascustomscount+$nocustomscount%></b><br>
-Number of professors who have logged In: <b><%=$hascustomscount+$nocustomscount%></b><br>
+Number of professors who have logged in: <b><%=$hascustomscount+$nocustomscount%></b><br>
 Number of professors with custom surveys: <b><%=$hascustomscount%></b><br>
 <img src="<%=$server_wcespath%>media/graphs/pusagegraph.php?neverloggedin=<%=$notloggedincount%>&custom=<%=$hascustomscount%>&nocustom=<%=$nocustomscount%>" width=200 height=200><img src="<%=$server_wcespath%>media/graphs/pusagelegend.gif" width=133 height=49><br>
 <p>&nbsp;</p>
@@ -58,9 +95,9 @@ Number of professors with custom surveys: <b><%=$hascustomscount%></b><br>
 
 <%
 
-printproflist("professors who have not logged in",$notloggedin,$notloggedincount);
-printproflist("professors who have logged in without creating custom questions",$nocustoms,$nocustomscount);
-printproflist("professors who have logged in and created custom questions",$hascustoms,$hascustomscount);
+printproflist("Professors who have not logged in during the past semester",$notloggedin,$notloggedincount);
+printproflist("Professors who have logged in without creating custom questions",$nocustoms,$nocustomscount);
+printproflist("Professors who have logged in and created custom questions",$hascustoms,$hascustomscount);
 
 mysql_query("DROP TABLE currentprofessors");
 mysql_query("DROP TABLE currentclasses");
