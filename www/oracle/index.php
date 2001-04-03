f393d425cf9505fe55b30980d22fc49a705f256c
Russell Yanofsky <russ@yanofsky.org>
2001-04-03 01:47:44 +0000

    Added TA questions

--- www/oracle/index.php
+++ www/oracle/index.php
@@ -1,3 +1,50 @@
-<%require_once("oracle/page.inc");page_top("SEAS Oracle Homepage","home.gif");%>      <P>      <FONT face="Verdana,Trebuchet MS" size=-1>      <P align=left><FONT color=red>NOTE:</FONT> <I>ALL INFORMATION UPDATED as       of Nov 11, 2000</I></P>      <P align=left><FONT color=red>      <UL>        <LI>To view evaluations, click the "come in" icon in the bottom left hand         corner.         <LI>We would like to hear from you, please fill out the feedback form! </LI></UL></FONT>      <P></P>      <P align=justify>Welcome to the SEAS Oracle! </p>  
-      <P align=justify>The SEAS Oracle has a vast amount of information about academics here       at the Fu Foundation School of engineering and Applied Science. This site contains the       largest compilation of Columbia Course/ Professor information. You’ll learn useful and       interesting facts about professors and courses, including background, research,       education and more. You’ll also see the results of those course evaluations you fill out       every semester.</P>      <P align=justify>Course Evaluations are used to improve the quality of education at universities across the nation and worldwide. Although the evaluations do not always reflect on courses and their professors accurately, they are still a necessary measure of quality and needed improvement. The Oracle can assist students in their selection of classes.</P>      
-      <P align=justify>This year we took last year's constructive feedback to heart and have worked to further improve this service to students and faculty. We have designed, constructed and implemented a database that will store information for each year by semester. Therefore, students and faculty will be able to view evaluation data semester to semester. This gives faculty and students a better picture of the class and the abilities of the professors.        </FONT><!-- End of main info ---><BR>      <HR width="100%">      <P align=center><FONT size=2>If you have any problems or questions       concerning this webpage, please email</FONT></P>      <P align=center><FONT size=2>the <A       href="mailto:wces@columbia.edu">webmaster</A>.</FONT></P><%page_bottom();%>
+<%
+  require_once("oracle/page.inc");
+  page_top("SEAS Oracle Homepage","home.gif");
+%>
+<font face="Verdana,Trebuchet MS" size="-1">
+<p align="left"><font
+color="red">NOTE:</font> <i>ALL INFORMATION UPDATED as of Nov 11,
+2000</i></p>
+
+<ul>
+<li><font color="red">To view evaluations, click the "come in" icon
+in the bottom left hand corner.</font></li>
+
+<li><font color="red">We would like to hear from you, please fill
+out the feedback form!</font></li>
+</ul>
+
+<p align="justify">Welcome to the SEAS Oracle!</p>
+
+<p align="justify">The SEAS Oracle has a vast amount of information
+about academics here at the Fu Foundation School of engineering and
+Applied Science. This site contains the largest compilation of
+Columbia Course/ Professor information. You&rsquo;ll learn useful
+and interesting facts about professors and courses, including
+background, research, education and more. You&rsquo;ll also see the
+results of those course evaluations you fill out every
+semester.</p>
+
+<p align="justify">Course Evaluations are used to improve the
+quality of education at universities across the nation and
+worldwide. Although the evaluations do not always reflect on
+courses and their professors accurately, they are still a necessary
+measure of quality and needed improvement. The Oracle can assist
+students in their selection of classes.</p>
+
+<p align="justify">This year we took last year's constructive
+feedback to heart and have worked to further improve this service
+to students and faculty. We have designed, constructed and
+implemented a database that will store information for each year by
+semester. Therefore, students and faculty will be able to view
+evaluation data semester to semester. This gives faculty and
+students a better picture of the class and the abilities of the
+professors. </p>
+</font>
+<hr>
+<p align="center"><font size="2">If you have any problems or
+questions concerning this webpage, please email the <a href=
+"mailto:wces@columbia.edu">webmaster</a>.</font></p>
+
+<% page_bottom(); %>
