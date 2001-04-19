542e22a9cab0ffe88ed38b0efb06bc4e119d7458
Russell Yanofsky <russ@yanofsky.org>
2001-04-19 20:16:28 +0000

    Added professor listing, secure login. Opened for final evaluations

---
+++ www/wces/info/previewsurvey.php
@@ -0,0 +1,37 @@
+<%
+  require_once("wces/page.inc");
+  require_once("wces/login.inc");
+  require_once("wces/wces.inc");
+  require_once("wces/oldquestions.inc");
+
+  page_top("Survey Preview","1000");
+  
+  $db = wces_connect();
+
+  wces_Findquestionsets($db,"qs",$classid);
+  $result = db_exec("SELECT questionsetid FROM qs", $db, __FILE__, __LINE__);
+  $questionsets = array();
+  while($row = mysql_fetch_assoc($result))
+  $questionsets[] = $row["questionsetid"];
+  db_exec("DROP TABLE qs", $db, __FILE__, __LINE__);
+    
+  print("<p><strong><a href=\"classinfo.php?classid=$classid\">Back</a></strong></p>");
+
+  if (count($questionsets) > 0)
+  {
+    print("<form>");
+    foreach($questionsets as $k => $qid)
+    { 
+      $q = new OldQuestionWidget($db,$qid, 0, 0, "preview$k","prv",WIDGET_GET);
+      $q->display();
+    }  
+    print("</form>");
+  }
+  else
+  {
+    print("<p><b>No question sets found</b></p>");
+  }
+  print("<p><strong><a href=\"classinfo.php?classid=$classid\">Back</a></strong></p>");
+   
+  page_bottom();
+%>
---
+++ www/wces/info/profinfo.php
@@ -0,0 +1,43 @@
+<%
+  require_once("wces/page.inc");
+  require_once("wces/login.inc");
+  require_once("wces/wces.inc");
+  page_top("Professor Profile","1000");
+  
+$db = wces_connect();
+if ($professorid && $info = db_getrow($db,"professors",Array("professorid" => $professorid),0))
+{
+  extract($info);
+  $cunix = db_getvalue($db,"users",Array("userid" => $userid),"cunix");
+  
+  
+  print("<h3>$name</h3>\n");
+  
+  if ($picname) print("<p><img src=\"/oracle/prof_images/$picname\"></p>");
+  if ($statement) print("<h4>Statement</h4>\n<p>$statement</p>\n");
+  if ($profile) print("<h4>Profile</h4>\n<p>$profile</p>\n");
+  if ($education) print("<h4>Education</h4>\n<p>$education</p>\n");
+  if ($email || $url ||$cunix) print("<h4>Contact Information</h4>\n");
+  if ($email) print ("<p><a href=\"mailto:$email\">$email</a></p>\n");
+  if ($url) print ("<p><a href=\"$url\">$url</a></p>\n");
+  if ($cunix) print ("<p>CUNIX ID: <a href=\"http://www.columbia.edu/cgi-bin/lookup.pl?$cunix\">$cunix</a></p>\n");
+  
+  print ("<h4>Classes</h4>\n<UL>\n");
+  $classes = mysql_query("SELECT cl.classid, cl.section, cl.year, cl.semester, c.code, c.name, s.code as scode FROM classes as cl LEFT JOIN courses AS c USING (courseid) LEFT JOIN subjects AS s USING (subjectid) WHERE cl.professorid = '$professorid' ORDER BY cl.year DESC, cl.semester DESC LIMIT 50",$db);
+  while ($class = mysql_fetch_assoc($classes))
+  {
+    $classid = $section = $year = $scode = $code = $name = $section = "";
+    extract($class);
+    print ("  <LI><A HREF=\"classinfo.php?classid=$classid\">" . ucfirst($semester) . " $year  - $scode$code <i>$name</i> (section $section)</A></LI>\n");
+  }
+  print("</UL>\n");
+}
+
+  page_bottom();
+%>
+
+
+
+
+
+
