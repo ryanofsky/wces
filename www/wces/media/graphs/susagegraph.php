943a2c3657c3a0fea80dd89535717d9cf7045fe0
Russell Yanofsky <russ@yanofsky.org>
2001-04-20 22:06:06 +0000

    Added TA questions

--- www/wces/media/graphs/susagegraph.php
+++ www/wces/media/graphs/susagegraph.php
@@ -1,4 +1,7 @@
-<%include('wces/server.inc');
+<%
+error_reporting (0);
+
+include('wces/server.inc');
 Header("Content-type: image/gif");
 
 if (function_exists('imagecreate'))
@@ -10,15 +13,26 @@ $RGB["wcesyellow"] = Array(0xFF,0xFB,0x18);
 $a = array(array(0,0),array(0+$blank,0+$filled));
 
 phpplot(array(
-    "cubic" => true,    "transparent" => true,
-    "colorset" => array("lred","wcesyellow"),    "title" => false,    "size"=> array(200,200) ));
+    "cubic" => true,
+    "transparent" => true,
+    "colorset" => array("lred","wcesyellow"),
+    "title" => false,
+    "size"=> array(200,200) ));
     
 phpdata($a);
-phpdraw("piegraph", array(  "scale" => 1.5,  "showlabel" => false,  "piemode" => "explode",  "drawsets" => array(1)));$fname = tempnam($server_tempdir, "graph");
+
+phpdraw("piegraph", array(
+  "scale" => 1.5,
+  "showlabel" => false,
+  "piemode" => "explode",
+  "drawsets" => array(1)
+));
+
+$fname = tempnam($server_tempdir, "graph");
 phpshow($fname,"gif");
 readfile($fname);
 unlink($fname);
 }
 else
-  readfile('http://oracle.yahweh.dyndns.org' . server_getrequest());
+  readfile('http://160.39.128.25:63' . server_getrequest());
 %>
