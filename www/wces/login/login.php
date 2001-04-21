4547e345922201963e93cdf018194679598a7a23
Russell Yanofsky <russ@yanofsky.org>
2001-04-21 04:24:49 +0000

    Some early work on WBES including a database schema, site prototype, and professor response pages.

--- www/wces/login/login.php
+++ www/wces/login/login.php
@@ -1,4 +1,4 @@
-<%
+<?
   require_once("wces/login.inc");
   require_once("wces/page.inc");
   page_top("Login Page");
@@ -21,28 +21,28 @@
     print("<p><b><font color=red>Invalid User Name or Password. Please Try Again</font></b></p>");
   else
     print("<p><b>Enter your CUNIX Username and Password below to log in:</b></p>");
-%>
-<form name="login" method="post" action="<%=$server_wcespath%>login/bounce.php">
+?>
+<form name="login" method="post" action="<?=$server_wcespath?>login/bounce.php">
 <table>
-<tr><td>Username:</td><td><input name="uni" type="text" value="<%=htmlspecialchars($uni)%>" size="20"></td></tr>
+<tr><td>Username:</td><td><input name="uni" type="text" value="<?=htmlspecialchars($uni)?>" size="20"></td></tr>
 <tr><td>Password:</td><td><input name="password" type="password" value="" size="20"></td></tr>
 <tr><td colspan=2><center><input type="submit" value="Log In" name=submit></center></td></tr>
 </table>
-<input type="hidden" name=destination value="<%=htmlspecialchars($destination)%>">
-<input type="hidden" name=requestedstatus value="<%=htmlspecialchars($requestedstatus)%>">
+<input type="hidden" name=destination value="<?=htmlspecialchars($destination)?>">
+<input type="hidden" name=requestedstatus value="<?=htmlspecialchars($requestedstatus)?>">
 </form>
 
-<% if (!server_secure()) { %>
+<? if (!server_secure()) { ?>
 
 <h3>Security Alert</h3>
 
 <p>The information sent in the form above is not encrypted, and could potentially be intercepted
-by someone monitoring http traffic on your network. <a href="http://www.columbia.edu/acis/security/">AcIS</a>recommends that you use encryption whenever possible. To start an encrypted session, click the link below:</p><%$params="destination=" . urlencode($destination) . "&requestedstatus=" .urlencode($requestedstatus) . "&uni=" . urlencode($uni); %>
+by someone monitoring http traffic on your network. <a href="http://www.columbia.edu/acis/security/">AcIS</a>recommends that you use encryption whenever possible. To start an encrypted session, click the link below:</p><?$params="destination=" . urlencode($destination) . "&requestedstatus=" .urlencode($requestedstatus) . "&uni=" . urlencode($uni); ?>
 
 <ul>
-  <li><a href="<%=$server_sbase . $server_wcespath . "login/login.php?destination=" . urlencode($destination) . "&requestedstatus=" .urlencode($requestedstatus) . "&uni=" . urlencode($uni) . "&SESSION_TRANSFER_ID=" . urlencode(session_id())%>">Secure Server</a></li>
+  <li><a href="<?=$server_sbase . $server_wcespath . "login/login.php?destination=" . urlencode($destination) . "&requestedstatus=" .urlencode($requestedstatus) . "&uni=" . urlencode($uni) . "&SESSION_TRANSFER_ID=" . urlencode(session_id())?>">Secure Server</a></li>
 </ul>  
 
-<% }
+<? }
   page_bottom();
-%>
+?>
