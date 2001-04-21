8ca833075418589d6c8ce7661e0e693963e569a5
Russell Yanofsky <russ@yanofsky.org>
2001-04-21 22:33:36 +0000

    Added TA questions

--- www/wces/info/profinfo.php
+++ www/wces/info/profinfo.php
@@ -10,7 +10,6 @@ if ($professorid && $info = db_getrow($db,"professors",Array("professorid" => $p
   extract($info);
   $cunix = db_getvalue($db,"users",Array("userid" => $userid),"cunix");
   
-  
   print("<h3>$name</h3>\n");
   
   if ($picname) print("<p><img src=\"/oracle/prof_images/$picname\"></p>");
@@ -20,15 +19,30 @@ if ($professorid && $info = db_getrow($db,"professors",Array("professorid" => $p
   if ($email || $url ||$cunix) print("<h4>Contact Information</h4>\n");
   if ($email) print ("<p><a href=\"mailto:$email\">$email</a></p>\n");
   if ($url) print ("<p><a href=\"$url\">$url</a></p>\n");
-  if ($cunix) print ("<p>CUNIX ID: <a href=\"http://www.columbia.edu/cgi-bin/lookup.pl?$cunix\">$cunix</a></p>\n");
+  if ($cunix) print ("<p>CUNIX ID: <a href=\"ldap.php?uni=$cunix\">$cunix</a></p>\n");
   
   print ("<h4>Classes</h4>\n<UL>\n");
-  $classes = mysql_query("SELECT cl.classid, cl.section, cl.year, cl.semester, c.code, c.name, s.code as scode FROM classes as cl LEFT JOIN courses AS c USING (courseid) LEFT JOIN subjects AS s USING (subjectid) WHERE cl.professorid = '$professorid' ORDER BY cl.year DESC, cl.semester DESC LIMIT 50",$db);
+
+  wces_Findquestionsets($db,"qs");
+
+  $classes = db_exec("
+
+  SELECT cl.classid, cl.section, cl.year, cl.semester, c.code, c.name, s.code as scode, COUNT(qs.classid) AS cnt
+  FROM classes as cl
+  LEFT JOIN courses AS c USING (courseid)
+  LEFT JOIN subjects AS s USING (subjectid)
+  LEFT JOIN qs ON (qs.classid = cl.classid)
+  WHERE cl.professorid = '$professorid'
+  GROUP BY cl.classid
+  ORDER BY cl.year DESC, cl.semester DESC LIMIT 50",$db,__FILE__,__LINE__);
+  
   while ($class = mysql_fetch_assoc($classes))
   {
-    $classid = $section = $year = $scode = $code = $name = $section = "";
+    $cnt = $classid = $section = $year = $scode = $code = $name = $section = "";
     extract($class);
-    print ("  <LI><A HREF=\"classinfo.php?classid=$classid\">" . ucfirst($semester) . " $year  - $scode$code <i>$name</i> (section $section)</A></LI>\n");
+    print ("  <LI><A HREF=\"classinfo.php?classid=$classid\">" . ucfirst($semester) . " $year  - $scode$code <i>$name</i> (section $section)</A>");
+    if($cnt > 0) print(" - <a href=\"previewsurvey.php?classid=$classid\">Preview Survey</a>");
+    print("</LI>\n");
   }
   print("</UL>\n");
 }
