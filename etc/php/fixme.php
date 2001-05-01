bdafb3d92ebceb1fd7566c81067f25c317695832
Russell Yanofsky <russ@yanofsky.org>
2001-05-01 15:43:52 +0000

    Added TA & ABET reporting.

--- etc/php/fixme.php
+++ etc/php/fixme.php
@@ -1,50 +1,15 @@
 <%
 
-/*
-
-Jackie,
-
-The following people have been "unenrolled" from any classes that they did not survey. As long as they have filled out at least one survey they will appear on the list of people who completed all of their surveys.
-
-acc40
-bg171
-cmt25
-djh31
-drt14
-gb203
-hdr25
-jes141
-kk357
-hdr25
-lkj10
-nt102
-pgs54
-rj220
-sc1204
-sy192
-wl318
-yf91
-yk478
-
-One of these people, hdr25, said that the system was missing these two courses, Cell Biology (BIOL W4041) and Nucleic acids and Protein
-synthesis BCHMG4026. These courses are not being evaluated through WCES.
-
-I did not realize the problem was as widespread of this. Normally I wouldn't like to make changes to the login code during the middle of evaluations because it is so easy to break and so hard to debug. But I added an extra step in the login process that will clear out any old enrollments as the person logs in and keep only the newest data which is obtained directly from AcIS. Hopefully this will put an end to most of these login problems.
-
-
-*/
-
 require_once("wces/general.inc");
 require_once("wces/wces.inc");
 
 
 
 $db = wces_connect();
-db_exec("lkdjlk",$db,__FILE__, __LINE__);
 
 $questionperiodid = wces_Findquestionsetsta($db,"qsets");
 
-$names = array("acc40", "bg171", "cmt25", "djh31", "drt14", "gb203", "hdr25", "jes141", "kk357", "lkj10", "nt102", "pgs54", "rj220", "sc1204", "sy192", "wl318", "yf91", "yk478");
+$names = array("sms117", "jpm53", "ki126");
 foreach($names as $name)
 {
   $db_debug = true;
