d9a5507418a38ee62b438720c6638f9c39d7f540
cvs2git <>
2001-05-16 23:48:15 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.4.2'.
    Cherrypick from master 2001-05-16 23:48:13 UTC Russell Yanofsky <russ@yanofsky.org> 'Some early work on WBES including a database schema, site prototype, and professor response pages.':
        www/wces/about/feedback.php

---
+++ www/wces/about/feedback.php
@@ -0,0 +1,27 @@
+<?
+require_once("wces/page.inc");
+page_top("Feedback");
+
+param($comments);
+
+if (!$comments)
+{
+?>
+<p>Thank You for visiting the SEAS Oracle and WCES. In order for us to further improve this site, we would appreciate your feedback.</p>
+<form name=mail method=post action="feedback.php">
+<table>
+<tr><td valign=top>Email (optional) </td><td><input type=text name=email size=30></td></tr>
+<tr><td valign=top>Comments </td><td><textarea name=comments rows=7 cols=40></textarea></td></tr>
+<tr><td valign=top>&nbsp;</td><td><input type=submit name=submit value="Submit"></td></tr>
+</table>
+</form>
+<?
+}
+else
+{
+  if (!$email) $email = "Anonymous";
+  mail("wces@columbia.edu","WCES WEB FEEDBACK","Feedback from $email\n\n$comments");
+  print('Your comments have been sent to <a href="mailto:wces@columbia.edu">wces@columbia.edu</a>. Thank you for taking the time to help us.');
+};
+page_bottom();
+?>
