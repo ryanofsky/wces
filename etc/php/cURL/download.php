80b8f28938b555d0f5f9f1fbd77252f0d0fef9ed
Russell Yanofsky <russ@yanofsky.org>
2001-07-21 22:00:54 +0000

    Some early work on WBES including a database schema, site prototype, and professor response pages.

--- etc/php/cURL/download.php
+++ etc/php/cURL/download.php
@@ -4,7 +4,7 @@
 
 
 <?php 
-  if (!$url) $url = "http://www.mail2web.com";
+  if (!isset($url)) $url = "http://www.mail2web.com";
 ?>
 
 <form method=get>
@@ -19,6 +19,7 @@
 $tempdir = "/home/httpd/cgi-bin/123/russ";
 
 $fname = tempnam($tempdir, "booty");
+$fnamee = tempnam($tempdir, "booty");
 
 print("Downloading <b>$url</b> to <b>$fname</b></br><hr>");
 
@@ -26,29 +27,32 @@ flush();
 
 $ch = curl_init ($url);
 $fp = fopen ($fname, "w");
+$fpe = fopen ($fnamee, "w");
 
 curl_setopt ($ch, CURLOPT_FILE, $fp);
 curl_setopt ($ch, CURLOPT_HEADER, 1);
-
+curl_setopt ($ch, CURLOPT_VERBOSE, 1);
+curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 0);
+curl_setopt ($ch, CURLOPT_STDERR, $fpe);
 curl_exec ($ch);
 @curl_close ($ch);
 fclose ($fp);
-
-print("<pre>");
+fclose($fpe);
 
 $fp = fopen($fname,"r");
-
+print("<pre>");
 print(htmlspecialchars(fread($fp, 1000000)));
-
-fclose($fp);
-
 print("</pre><hr>");
-
+fclose($fp);
 unlink($fname);
 
+$fpe = fopen($fnamee,"r");
+print("<h1>Errors</h1>");
+print("<pre>" . htmlspecialchars(fread($fpe, 1000000)) . "</pre>");
+fclose($fpe);
+unlink($fnamee);
+
 ?>
-
-
 
 </body>
 </html>
