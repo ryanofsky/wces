78eb94f6b266ae5f9c232bb0ace7f50ede4cc537
Russell Yanofsky <russ@yanofsky.org>
2001-08-09 04:34:59 +0000

    WBES 0.1 - New login code and improved session management, more fleshed out site design, separation of WBES and WCES.

--- www/wces/index.php
+++ www/wces/index.php
@@ -1,37 +1,73 @@
 <?
+  require_once("wces/login.inc");
+  login_protect(login_any);
+  
   require_once("wces/page.inc");
-  page_top("WCES Mission");
+  page_top("The WCES");
+  
+  print("<h3>Welcome " . login_getname() . ",</h3>\n");
+  $status = login_getstatus();
+
+  $onemenu = true;
+
+  if ($status & login_administrator)
+  {
+    if ($onemenu) $onemenu = false; else print("<h4>Administrator Options</h4>");
 ?>
-
-<table><tr><td width=590>
-
-<h3>System Overview</h3>
-
-<img src="media/pencil.gif" width="74" height="42" align="left" valign="top">
-<p>The <b>Web-Based Course Evaluation System</b> (WCES) seeks to give students greater ease and peace of mind in evaluating their classes while allowing for greater flexibility and data integrity from the perspective of the evaluations' administrators. The course feedback gathered through this mechanism should be useful for professors and students alike.</p>
-
-<p>The WCES is <b>COMPLETELY ANONYMOUS!!</b> If you have any questions, please email your concerns to <a href=mailto:wces@columbia.edu>wces@columbia.edu</a></p>
-
-<h3>Getting Started</h3>
-
-<p><b>Students.</b> Choose "Complete a Survey" under the students section at the left
- to evaluate your classes for this semester. Choose "View Survey Results" to see the results
- of past surveys and to learn more about Columbia's classes and professors.</p>
-
-<p><b>Professors.</b> Choose "Home" under the professors section at the left to log in
-to WCES. Once you have logged in you will be able to add custom questions to upcoming surveys
-and see results of already completed ones. You can edit questions and log in during non-evaluation period.</p>
-
-<p><b>Macintosh Users!</b> By default, Internet Explorer for Macintosh will
-refuse to connect to our secure site because its certificate was issued
-by a <a href="http://www.columbia.edu/acis/rad/columbiaca/">nonstandard</a>
-certification authority. You can log on securely using a netscape browser
-or log on without encryption by clicking
-<a href="login/login.php">here</a>.</p>
-
-
-</td></tr></table>
-
+<p><img align=right src="<?=$wces_path?>media/admin.jpg" width=125 height=125>Here is a list of the options currently available for administrators:</p>
+<ul>
+  <li><a href="<?=$wces_path?>administrators/susage.php<?=$QSID?>">Student Usage</a> - See a report on student usage of WCES during the current question period.</li>
+  <li><a href="<?=$wces_path?>administrators/pusage.php<?=$QSID?>">Professor Usage</a> - See a detailed report of professor usage of WCES during the current question period.</li>
+  <li><a href="<?=$wces_path?>administrators/pstats.php<?=$QSID?>">Professor Listing</a> - View and edit information about professors.</li>
+  <li><a href="<?=$wces_path?>administrators/massmail.php<?=$QSID?>">Mass Mail</a> - Send reminder and thank-you emails to students.</li>
+  <li><a href="<?=$wces_path?>administrators/fakelogin.php<?=$QSID?>">Fake Login</a> - Log onto WCES as another user.</li>
+  <li><a href="<?=$wces_path?>administrators/enrollment.php<?=$QSID?>">Enrollment Viewer</a> - See a student's class enrollments.</li>
+  <li><a href="<?=$wces_path?>administrators/import.php<?=$QSID?>">Data Upload</a> - Load text files into the database.</li>
+  <li><a href="<?=$wces_path?>administrators/semester.php<?=$QSID?>">New Semester Initialization</a> - Upload registrar data for a new question period.</li>
+</ul>
 <?
+  }
+  
+  if ($status & login_professor)
+  {
+    if ($onemenu) $onemenu = false; else print("<h4>Professor Options</h4>");
+    
+    $pimg = "<img align=right src=\"${wces_path}media/professor_small.gif\" width=98 height=110>";
+    
+    if (!($status & login_knownprofessor))
+    {
+?>
+<p><?=$pimg?>Before you will be able to use WCES, your CUNIX account has to be associated
+with your listing in the SEAS course database. You can use this 
+<a href="<?=$wces_path?>login/profsearch.php<?=$QSID?>">search form</a> to find
+your listing and save the association.</p>
+<?
+    }
+    else
+    {
+?>
+<p><?=$pimg?>Here is a list of the options currently available for professors:</p>
+<ul>
+  <li><a href="<?=$wces_path?>professors/editsurveys.php<?=$QSID?>">Upcoming Surveys</a> - Edit or preview the questions your students will see in the upcoming question period.</li>
+  <li><a href="<?=$wces_path?>professors/seeresults.php<?=$QSID?>">Survey Sesults</a> - View the results of past surveys</li>
+  <li><a href="<?=$wces_path?>professors/infoedit.php<?=$QSID?>">Edit Profile</a> - Edit the information that displayed in the SEAS Oracle. You can update your personal statement, add a link to your homepage, or even upload a new photo.</li>
+</ul>
+<?
+    }
+  }
+
+  if ($status & login_student)
+  {
+    if ($onemenu) $onemenu = false; else print("<h4>Student Options</h4>");
+?>
+<p><img align=right src="<?=$wces_path?>media/student.gif" width=99 height=99>Choose a class to evaluate from the list below.</p>
+<ul>
+  <li><a href="<?=$wces_path?>students/survey.php?surveyid=1<?=$ASID?>">Survey 1</a></li>
+  <li><a href="<?=$wces_path?>students/survey.php?surveyid=2<?=$ASID?>">Survey 2</a></li>
+</ul>
+<p>Sick of automated emails? Adjust your <a href="<?=$wces_path?>students/optout.php<?=$QSID?>">email settings</a>.</p>
+<?
+  }  
+  
   page_bottom();
 ?>
