3ea0d52660ffbc4ac629d800ce157f39216f4b87
Russell Yanofsky <russ@yanofsky.org>
2001-08-10 05:02:54 +0000

    WBES 0.1 - New login code and improved session management, more fleshed out site design, separation of WBES and WCES.

--- www/test/logout.php
+++
@@ -1,8 +0,0 @@
-<?
-  require_once("wces/general.inc");
-  require_once("test/test.inc");
-  param($status);
-  test_top("Log Out",$status);
-  print("<h3>You are now logged out.</h3>");
-  test_bottom();
-?>
