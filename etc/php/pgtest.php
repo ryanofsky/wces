bbcae13ad5c2e40c0a6c050fefa1abffe5d458ff
Russell Yanofsky <russ@yanofsky.org>
2001-08-10 05:02:52 +0000

    WBES 0.1 - New login code and improved session management, more fleshed out site design, separation of WBES and WCES.

---
+++ etc/php/pgtest.php
@@ -0,0 +1,17 @@
+<?
+
+require_once("wbes/server.inc");
+require_once("wbes/postgres.inc");
+
+$db_debug = true;
+$db = server_pginit("testing");
+
+$result = pg_query("SELECT * FROM test", $db, __FILE__, __LINE__);
+
+for($i=0; $row = @pg_fetch_array($result,$i); ++$i) 
+{ 
+  print("let = " . $row["let"] . "\n");
+} 
+
+
+?>
