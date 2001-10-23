0d8d3a2db90f20745d6a3fe9bf50dbdbf03e9de7
Russell Yanofsky <russ@yanofsky.org>
2001-10-23 01:09:46 +0000

    change login code to use the postgres data

--- www/wces/login/profbounce.php
+++ www/wces/login/profbounce.php
@@ -1,9 +1,21 @@
 <?
   require_once("wbes/server.inc");
+  require_once("wbes/postgres.inc");
   require_once("wces/login.inc");
   login_protect(login_professor);
   param($url);
   if (!$url) $url = $wces_path . "index.php";
-  if ($profid) { login_saveprofid($profid); }
+  if (isset($user_id))
+  {
+    $curuser = login_getuserid();
+    $user_id = (int)$user_id;
+    if ($curuser != $user_id)
+    {
+      wces_connect();
+      $r = pg_query("SELECT cunix_associate($curuser,$user_id)", $wces, __FILE__, __LINE__);
+      $user_id = pg_result($r, 0, 0);
+      if ($user_id) login_update($user_id); else die("Failed to update cunix association.");
+    } 
+  }
   redirect($url)
 ?>
