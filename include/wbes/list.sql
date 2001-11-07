204a7285e2c9fd7c865c4e7bb9e946367f46437e
Russell Yanofsky <russ@yanofsky.org>
2001-11-07 07:22:58 +0000

    work on load and save support functions

--- include/wbes/list.sql
+++ include/wbes/list.sql
@@ -1,33 +1,284 @@
 DROP TABLE lists;
-DROP SEQUENCE list_ids;
-DROP SEQUENCE list_heads;
 DROP TABLE list_items;
-DROP FUNCTION list_update (integer, integer);
-DROP FUNCTION list_revision(integer, integer);
+DROP FUNCTION list_merge(INTEGER, INTEGER, INTEGER, INTEGER);
 
 CREATE TABLE lists
 (
   list_id INTEGER NOT NULL PRIMARY KEY DEFAULT NEXTVAL('list_ids'),
-  parent INTEGER
-  branch INTEGER,
-  revision INTEGER,
-  merged INTEGER REFERENCES lists(list_id),
-  modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  parent INTEGER,
+  branch INTEGER NOT NULL,
+  revision INTEGER NOT NULL,
+  merged INTEGER,
   UNIQUE(parent, branch, revision)
 );
 
 CREATE SEQUENCE list_ids INCREMENT 1 START 1;
-CREATE SEQUENCE list_b1s INCREMENT 1 START 1;
+CREATE SEQUENCE list_branch1s INCREMENT 1 START 1;
 
 CREATE TABLE list_items
 (
   list_item_id INTEGER PRIMARY KEY DEFAULT NEXTVAL('list_item_ids'),
   list_id INTEGER NOT NULL,
   ordinal SMALLINT NOT NULL,
-  item_id INTEGER
+  item_id INTEGER NOT NULL
 );
 
 CREATE INDEX list_idx ON list_items(list_id);
+
+-- with integer arrays, functions here use the same resource management convention used in Microsoft COM. Arrays passed as arguments into a function must be cleaned up by that function. arrays returned by functions are cleaned up by the caller.
+
+CREATE FUNCTION list_merge(INTEGER, INTEGER, INTEGER, INTEGER) RETURNS INTEGER AS '
+  DECLARE
+    common_id    ALIAS FOR $1;
+    primary_id   ALIAS FOR $2;
+    secondary_id ALIAS FOR $3;
+    new_id       ALIAS FOR $4;
+    rec RECORD;
+    ord INTEGER;
+    ck INTEGER;
+    nk INTEGER;
+  BEGIN
+    -- Create new list based on primary but without items that were deleted in the secondary list. 
+   
+    ord := 0;
+    FOR rec IN 
+      SELECT li.item_id
+      FROM list_items AS li
+      WHERE
+        li.list_id = primary_id
+      AND NOT -- deleted
+      (
+        EXISTS (SELECT 1 FROM list_items AS c WHERE c.list_id = common_id AND c.item_id = li.item_id)
+        AND
+        NOT EXISTS (SELECT 1 FROM list_items AS h WHERE h.list_id = secondary_id AND h.item_id = li.item_id)
+      )
+      ORDER BY li.ordinal
+    LOOP
+      INSERT INTO list_items(list_id, ordinal, item_id) VALUES (new_id, ord, rec.item_id);
+      ord := ord + 1;      
+    END LOOP;
+    
+    -- Insert new items from secondary into the new list
+
+    ord := 0;  
+    FOR rec IN SELECT ordinal, item_id FROM list_items WHERE list_id = secondary_id ORDER BY ordinal LOOP
+
+      SELECT INTO ck ordinal FROM list_items WHERE list_id = common_id AND item_id = rec.item_id;
+      IF NOT FOUND THEN ck := NULL; END IF;
+
+      SELECT INTO nk ordinal FROM list_items WHERE list_id = new_id AND item_id = rec.item_id;
+      IF NOT FOUND THEN nk := NULL; END IF;
+
+      IF ck IS NULL AND nk IS NULL THEN -- needs to be inserted
+        UPDATE list_items SET ordinal = ordinal + 1 WHERE list_id = new_id AND ordinal >= ord;
+        INSERT INTO list_items(list_id, ordinal, item_id) VALUES (new_id, ord, rec.item_id);
+        nk := ord;
+      END IF;
+
+      IF (nk IS NOT NULL) THEN ord := nk + 1; END IF;
+   
+    END LOOP;
+    RETURN 1;
+  END;  
+' LANGUAGE 'plpgsql';
+
+CREATE FUNCTION ia_test(INTEGER) RETURNS ia_data%ROWTYPE AS '
+  SELECT * FROM ia_data WHERE ia_id = $1
+' LANGUAGE 'sql';
+
+-- helper function used by other functions, sql ugliness
+
+CREATE FUNCTION list_max(INTEGER, INTEGER, INTEGER, BOOLEAN) RETURNS INTEGER AS '
+  DECLARE
+    parent_     ALIAS FOR $1;
+    branch_     ALIAS FOR $2;
+    maxrevision ALIAS FOR $3;
+    lock        ALIAS FOR $4;
+    rec RECORD
+    r   INTEGER;
+    ret INTEGER := NULL;
+  BEGIN
+    IF lock AND maxrevision IS NOT NULL THEN
+      RAISE EXCEPTION ''list_maxrevision is not implemented for the case where a maximum revision is set and locking is requested.'';
+    END IF;
+    
+    -- stuff between the dashes is just the same code repeated again and again
+    -- in all the different variations
+    
+    IF parent_ IS NULL THEN
+      IF maxrevision IS NULL THEN
+    
+        -------------------------------------------------------------------------
+        SELECT INTO r MAX(revision) FROM lists WHERE parent IS NULL AND branch = branch_;
+        IF r IS NOT NULL THEN
+          IF lock THEN
+            SELECT INTO rec list_id, revision FROM lists WHERE parent IS NULL AND branch = branch_ AND revision >= r ORDER BY revision DESC FOR UPDATE;
+            ret := ia_new('{0,0}');
+            ia_set(ret,1,rec.list_id);
+            ia_set(ret,2,rec.revision);
+          ELSE
+            SELECT INTO list_id list_id FROM lists WHERE parent IS NULL AND branch = branch_ AND revision = r;
+            ret := ia_new('{0,0}');
+            ia_set(ret,1,list_id);
+            ia_set(ret,2,r);
+          END IF;
+        END IF;
+        -------------------------------------------------------------------------
+     
+      ELSE -- maxrevision IS NOT NULL
+    
+        -------------------------------------------------------------------------
+        SELECT INTO r MAX(revision) FROM lists WHERE parent IS NULL AND branch = branch_ AND revision <= maxrevision;
+        IF r IS NOT NULL THEN
+          SELECT INTO list_id list_id FROM lists WHERE parent IS NULL AND branch = branch_ AND revision = r;
+          ret := ia_new('{0,0}');
+          ia_set(ret,1,list_id);
+          ia_set(ret,2,r);
+        END IF;
+        -------------------------------------------------------------------------
+    
+      END IF;
+    ELSE -- parent_ IS NOT NULL
+      IF maxrevision IS NULL THEN
+    
+        -------------------------------------------------------------------------
+        SELECT INTO r MAX(revision) FROM lists WHERE parent IS parent_ AND branch = branch_
+        IF r IS NOT NULL THEN
+          IF lock THEN
+            SELECT INTO rec list_id, revision FROM lists WHERE parent = parent_ AND branch = branch_ AND revision >= r ORDER BY revision DESC FOR UPDATE;
+            ret := ia_new('{0,0}');
+            ia_set(ret,1,rec.list_id);
+            ia_set(ret,2,rec.revision);
+          ELSE
+            SELECT INTO list_id list_id FROM lists WHERE parent = parent_ AND branch = branch_ AND revision = r;
+            ret := ia_new('{0,0}');
+            ia_set(ret,1,list_id);
+            ia_set(ret,2,r);
+          END IF;
+        END IF;
+        -------------------------------------------------------------------------
+    
+      ELSE -- maxrevision IS NOT NULL
+    
+        -------------------------------------------------------------------------
+        SELECT INTO r MAX(revision) FROM lists WHERE parent = parent_ AND branch = branch_ AND revision <= maxrevision;
+        IF r IS NOT NULL THEN
+          SELECT INTO list_id list_id FROM lists WHERE parent = parent_ AND branch = branch_ AND revision = r;
+          ret := ia_new('{0,0}');
+          ia_set(ret,1,list_id);
+          ia_set(ret,2,r);
+        END IF;
+        -------------------------------------------------------------------------
+    
+      END IF;
+    END IF;
+  END;  
+' LANGUAGE 'plpgsql';
+
+CREATE FUNCTION list_latest(INTEGER, INTEGER, BOOLEAN, BOOLEAN, BOOLEAN) RETURNS INTEGER AS '
+  DECLARE
+    base       ALIAS FOR $1; -- ia index
+    d          ALIAS FOR $2;
+    readonly   ALIAS FOR $3;
+    beforeonly ALIAS FOR $4;
+    locklast   ALIAS FOR $5;
+    depth      INTEGER;
+    maxr       INTEGER;
+    lmax       INTEGER;
+    m          INTEGER;
+    last       INTEGER := 0;
+    revision   INTEGER;
+  BEGIN
+    revision := base;
+    depth := (ia_length(revision) - 1) / 3;
+
+    -- maxr is only needed because the postgres arrays cannot hold null values.
+    -- when this and other the other array problems that exist in Postgres 7.1.3
+    -- are finally fixed, this function can become cleaner and more efficient.
+
+    maxr  := ia_new('{}'); 
+    i := 1;
+    LOOP
+      EXIT WHEN i >= d AND (NOT beforeonly OR i >= depth);
+      PERFORM ia_set(maxr,i,ia_get(revision,i*3));
+      i := i + 1;
+    END LOOP;
+    
+    LOOP
+      EXIT WHEN d > depth;
+      tp := ia_get(revision,d*3-5);
+      tb := ia_get(revision,d*3-1);
+      tr := ia_get(maxr,d);
+      lmax := list_max(tp, tb, tr, locklast && d = depth);
+      IF lmax IS NULL THEN
+        IF readonly THEN
+          ia_set(maxr,d,NULL); -- truncate maxr at d
+          d := d - 1;
+          IF d < 1 THEN ia_delete(revision); revision := NULL; EXIT; END IF;
+          UPDATE ia_data SET data[d] = data[d] - 1 WHERE ia_id = maxr; -- equivalent: maxr[d] := maxr[d] - 1
+        ELSE
+          i := ia_clone(revision);
+          ia_set(i,d*3+1,NULL); -- truncate i at d*3+1
+          i := list_latest(i, d, 1, 0, 1);
+          IF i IS NULL THEN
+            RAISE EXCEPTION ''Unable to find any past revisions on the current branch.'';
+          END IF;
+          lmax := list_max(tp, tb, tr, locklast && d = depth);
+          IF lmax IS NULL THEN
+            IF ia_get(i,d*3) == 0 THEN r := 0; ELSE r := 1; END IF;
+            INSERT INTO lists(parent, branch, revision, merged) VALUES(tp, tb, r);
+            new_id := currval(''list_ids'');
+            ia_set(revision,d*3,r);
+            ia_set(revision,d*3-2,new_id);
+            IF r <> 0 THEN
+              last := new_id;
+              primary_id := ia_get(i,d*3-2);
+              secondary_id = tp;
+              j := d;
+              LOOP
+                j := j - 1;
+                EXIT WHEN j <= 1 OR ia_get(i,j*3) != 0; 
+              END LOOP;
+              common_id := ia_get(i,j*3-2);
+              array_merge(common_id, primary_id, secondary_id, new_id);
+            END IF;
+            d := d + 1;
+          END IF;
+        END IF;  
+      END IF;
+      IF lmax IS NOT NULL THEN 
+        maxr[d] := lmax[2];
+        revision[d*3-2] := lmax[1];
+        revision[d*3-1] := branch[d];
+        revision[d*3  ] := lmax[2];
+        IF lmax[2] <> 0 THEN last := lmax[1]; END IF;
+        d = d + 1;
+      END IF;
+    end loop
+    revision[d*3-2] := last;
+  END;
+' LANGUAGE 'plpgsql';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -324,90 +575,6 @@ CREATE FUNCTION list_newbranch(b1,r1)
     b1_ ALIAS FOR $1;
     list_id_ INTEGER;
   BEGIN
-  END;  
-' LANGUAGE 'plpgsql';
-
-
-CREATE FUNCTION list_update (integer, integer) RETURNS INTEGER AS '
-  DECLARE
-    list_head_ ALIAS FOR $1;
-    branch_    ALIAS FOR $2;
-    rec RECORD;
-    headid INTEGER;
-    headrev INTEGER;
-    branchid INTEGER;
-    branchrev INTEGER;
-    commonid INTEGER;
-    newid INTEGER;
-    ord INTEGER;
-    ck INTEGER;
-    nk INTEGER;
-
-  BEGIN
-    SELECT INTO rec list_id, revision FROM lists WHERE list_head = list_head_ AND branch = 0 ORDER BY revision DESC LIMIT 1;
-    IF NOT FOUND THEN RETURN NULL; END IF;
-    headid := rec.list_id;
-    headrev := rec.revision;
-    
-    SELECT INTO rec list_id, revision FROM lists WHERE list_head = list_head_ AND branch = branch_ ORDER BY revision DESC LIMIT 1;
-    IF NOT FOUND THEN RETURN NULL; END IF;
-    IF rec.revision = headrev THEN RETURN 1; END IF;
-    branchid := rec.list_id;
-    branchrev := rec.revision;
-    
-    SELECT INTO rec list_id, revision FROM lists WHERE list_head = list_head_ AND branch = 0 AND revision = branchrev;
-    IF NOT FOUND THEN RETURN NULL; END IF;
-    commonid := rec.list_id;
-
-    INSERT INTO lists(list_head, revision, branch) VALUES(list_head_, headrev, branch_);
-    newid := currval(''list_ids'');
-
-    -- Create new list based on branch but without items that were deleted in the head list. 
-    
-    ord := 0;
-    FOR rec IN 
-      SELECT li.item_id
-      FROM list_items AS li
-      WHERE
-        li.list_id = branchid
-      AND NOT -- deleted
-      (
-        EXISTS (SELECT 1 FROM list_items AS c WHERE c.list_id = commonid AND c.item_id = li.item_id)
-        AND
-        NOT EXISTS (SELECT 1 FROM list_items AS h WHERE h.list_id = headid AND h.item_id = li.item_id)
-      )
-      ORDER BY li.ordinal
-    LOOP
-      INSERT INTO list_items(list_id, ordinal, item_id) VALUES (newid, ord, rec.item_id);
-      ord := ord + 1;      
-    END LOOP;
-    
-    -- Insert new items from HEAD into the new list
-
-    ord := 0;  
-    FOR rec IN SELECT ordinal, item_id FROM list_items WHERE list_id = headid ORDER BY ordinal LOOP
-
-      SELECT INTO ck ordinal FROM list_items WHERE list_id = commonid AND item_id = rec.item_id;
-      IF NOT FOUND THEN ck := NULL; END IF;
-
-      SELECT INTO nk ordinal FROM list_items WHERE list_id = newid AND item_id = rec.item_id;
-      IF NOT FOUND THEN nk := NULL; END IF;
-
-      IF ck IS NULL AND nk IS NULL THEN -- needs to be inserted
-        UPDATE list_items SET ordinal = ordinal + 1 WHERE list_id = newid AND ordinal >= ord;
-        INSERT INTO list_items(list_id, ordinal, item_id) VALUES (newid, ord, rec.item_id);
-        nk := ord;
-      END IF;
-
-      IF (nk IS NOT NULL) THEN ord := nk + 1; END IF;
-   
-    END LOOP;
-    RETURN newid;
-    
-    -- TODO: come back to this code in a few weeks, and see if you can
-    -- make this code less convoluted. I like the behavior, but I suspect
-    -- a more direct and efficient implmentation is possible.
-    
   END;  
 ' LANGUAGE 'plpgsql';
 
