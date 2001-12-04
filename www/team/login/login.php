47ba007449b0bd8863611ed45ec51b8a6dbcc819
Russell Yanofsky <russ@yanofsky.org>
2001-12-04 08:43:32 +0000

    first team developer site

---
+++ www/team/login/login.php
@@ -0,0 +1,28 @@
+<?
+
+  require_once("wbes/server.inc");
+  require_once("team/login.inc");
+
+  param($uni,"");                            // username
+  param($pw,"");                             // password
+  param($url,"");                            // requested url
+  $rs = (int) rparam($rs,0);                 // requested status (status needed to access requested page)
+  $de = (boolean) rparam($de,false);         // disable encryption
+
+  $badpw = false; $message = "";
+  if($uni || $pw) $badpw = !login_validate($uni, $pw);
+ 
+  if ($badpw)
+    $message = "<p><b><font color=red>Invalid username or password</font></b></p>";
+  else
+  {
+    $status = login_getstatus();
+    if ($rs & $status)
+    {
+      if ($url) sloppyredirect($url);
+    }
+  }
+
+  login_prompt($url, $rs, $de, $uni);
+
+?>
