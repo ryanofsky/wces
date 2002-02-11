fc4cb670e055e66ba7d71ff7000c42ae7a9703e5
Russell Yanofsky <russ@yanofsky.org>
2002-02-11 08:55:35 +0000

    tests new interface

--- etc/php/dynlist.php
+++ etc/php/dynlist.php
@@ -1,21 +1,32 @@
 <?
- require_once("wces/page.inc");
- require_once("widgets/widgets.inc");
- require_once("widgets/dynamiclist.inc");
- require_once("widgets/basic.inc");
+  require_once("wces/page.inc");
+  require_once("widgets/widgets.inc");
+  require_once("widgets/dynamiclist.inc");
+  require_once("widgets/basic.inc");
+  
+  page_top("hello?");
  
- page_top("hello?");
-
- $form = new Form("form", "f", WIDGET_GET);
- $form->loadvalues();
- 
- $mc1 = new DynamicList(30,7,false,"mc1","f",WIDGET_GET);
- if ($form->isstale) $mc1->loadvalues(); else $mc1->items = array("check","the","cards","at","the","table");
- 
+  $form = new Form("form", "f", WIDGET_GET);
+  $form->loadvalues();
+  
+  $mc1 = new DynamicList(30,6,false,"mc1","f",WIDGET_GET);
+  $mc2 = new DynamicList(30,8,array("the", "winged", "merchant", "flies", "with", "eyes", "of", "fire"),"mc2","f",WIDGET_GET);
+  if ($form->isstale)
+  {
+    $mc1->loadvalues();
+    $mc2->loadvalues();
+  }
+  else
+  { 
+    $mc1->items = array("check","the","cards","at","the","table");
+    $mc1->keys = array(9,8,7,6,5,4);
+  }  
 ?>
 <form name="f" method=get>
 <? $form->display(); ?>
 <? $mc1->display(); ?>
+<hr>
+<? $mc2->display(); ?>
 <input type=submit name=submit value="Submit"><br><br>
 </form>
 <?
