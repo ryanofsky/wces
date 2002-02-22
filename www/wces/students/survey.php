413a20909482fa5926aa1723702c292a45f31d05
Russell Yanofsky <russ@yanofsky.org>
2002-02-22 10:32:23 +0000

    working student survey page

--- www/wces/students/survey.php
+++ www/wces/students/survey.php
@@ -1,79 +1,69 @@
 <?
 
+require_once("wbes/server.inc");
 require_once("wces/page.inc");
 require_once("wces/login.inc");
 require_once("wces/wces.inc");
-require_once("wbes/surveyeditor.inc");
+require_once("wbes/surveywidget.inc");
+require_once("wbes/component_text.inc");
+require_once("wbes/component_heading.inc");
+require_once("wbes/component_choice.inc");
+require_once("wbes/component_textresponse.inc");
+require_once("wbes/component_pagebreak.inc");
+
 login_protect(login_student);
 
-param($classid);
+param($class_id);
 param($save);
 
-if (!isset($db)) $db = wces_connect();
-$userid = login_getuserid();
-wces_GetCurrentQuestionPeriod($db, $questionperiodid, $description, $year, $semester);
+wces_connect();
 
-if ($classid)
+if ($class_id)
 {
-  $classid = (int)$classid;
-  $result = db_exec("
-    SELECT 1
-    FROM enrollments AS e
-    INNER JOIN groupings AS g ON e.classid = g.linkid AND g.linktype = 'classes'
-    INNER JOIN classes AS cl ON g.linkid = cl.classid AND year = '$year' AND semester = '$semester'
-    LEFT JOIN cheesyresponses AS cr ON cr.userid = e.userid AND cr.classid = e.classid AND cr.questionperiodid = '$questionperiodid'
-    WHERE e.classid = '$classid' AND e.userid = '$userid' AND cr.userid IS NULL
-  ", $db, __FILE__, __LINE__);
-  
-  if(mysql_num_rows($result) == 1)
+  $question_period_id = (int) pg_result(pg_query("SELECT get_question_period()", $wces, __FILE__, __LINE__),0,0);
+  $user_id = login_getuserid();
+  $class_id = (int)$class_id;
+  $result = pg_query("
+      SELECT t.topic_id, EXISTS (SELECT * FROM survey_responses AS sr WHERE sr.topic_id = t.topic_id AND sr.question_period_id = $question_period_id AND sr.user_id = $user_id)
+      FROM wces_topics AS t
+      INNER JOIN enrollments AS e ON e.user_id = $user_id AND e.class_id = t.class_id AND e.status = 1
+      WHERE e.class_id = $class_id
+  ", $wces, __FILE__, __LINE__);
+
+  if(pg_numrows($result) == 1 && pg_result($result,0,1) == 'f')
   {
-    $e = new SurveyEditor("prefix","f",WIDGET_POST);
-    $e->editclass = $classid;
-    $e->load();
-    
-    foreach(array_keys($e->survey->components) as $i)
-    {
-      $c = &$e->survey->components[$i];
-      $w = $c->getwidget("student_$i","f", WIDGET_POST);
-      $w->loadvalues();
-      $widgets[] = $w;
-    }
-    
-    if ($save)
-    {
-      db_exec("
-        INSERT INTO cheesyresponses (userid, classid, questionperiodid, dump) VALUES
-        ('$userid', '$classid', '$questionperiodid', '" . addslashes(serialize($HTTP_POST_VARS)) . "')", $db, __FILE__, __LINE__);
-        redirect($wces_path);
-        exit();
-    }
+    $topic_id = (int)pg_result($result,0,0);
+    $factories = array
+    (
+      new ChoiceFactory(),
+      new TextResponseFactory(),
+      new TextFactory(),
+      new HeadingFactory(),
+      new PageBreakFactory()
+    );
+    $q = new SurveyWidget($topic_id, 1, $user_id, $question_period_id, $factories, "prefix","f",WIDGET_POST);
+    $q->loadvalues();    
   }
   else
-    $classid = 0;
+    $class_id = 0;
 }
 
-page_top("Student Survey");
 
-if ($classid)
+if ($class_id)
 {
-  $result = db_exec("
-    SELECT CONCAT(s.code, c.code, ' ', c.name) AS name
-    FROM classes AS cl
-    INNER JOIN courses AS c USING (courseid)
-    INNER JOIN subjects AS s USING (subjectid)
-    WHERE cl.classid = '$classid'
-  ", $db, __FILE__, __LINE__);
-  print("<h3>" . mysql_result($result,0) . "</h3>");  
-  print("<form name=f method=post action=survey.php>\n");
-  print("<input type=hidden name=classid value=$classid>\n");
-  foreach(array_keys($widgets) as $i)
+  if ($q->finished)
   {
-    print("<div>");
-    $widgets[$i]->display();
-    print("</div>\n<br>\n");
+    if ($q->failure) exit();
+    redirect("{$wces_path}index.php$QSID");
   }
-  print('<input type=submit name=save value="Save Responses" class=tinybutton>');
-  print("</form>\n");
+  else
+  {
+    page_top("Student Survey");
+    print("<form name=f method=post>");
+    $q->display();
+    print("</form>");
+    page_bottom();
+  }
 }
 else
   print("Invalid Class ID.");
