087986d97a51f1b890221b11ec1393cfbba576b6
Russell Yanofsky <russ@yanofsky.org>
2002-04-10 22:23:03 +0000

    CREATE FUNCTION calls needed to use the library from postgres

---
+++ etc/postgres/install.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION int_distf(integer[], integer) RETURNS integer[] AS
+  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres/distribution.dll', 'int_distf'
+LANGUAGE 'c' WITH (iscachable);
+
+CREATE FUNCTION int_mdistf(integer[], integer[]) RETURNS integer[] AS
+  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres/distribution.dll', 'int_mdistf'
+LANGUAGE 'c' WITH (iscachable);
