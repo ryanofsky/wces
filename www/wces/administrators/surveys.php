7ed3ed1c4873a7e9297a6c14d45166f1a150ce7f
Russell Yanofsky <russ@yanofsky.org>
2002-04-21 22:55:11 +0000

    courses are now topics

--- www/wces/administrators/surveys.php
+++ www/wces/administrators/surveys.php
@@ -58,7 +58,11 @@ if ($q)
 
 }
 
-function topics_link($result, $class = false)
+define("LINK_LITERAL", 0);
+define("LINK_CLASS", 1);
+define("LINK_COURSE", 2);
+
+function topics_link($result, $type = LINK_LITERAL)
 {
   global $pagename;
   $n = pg_numrows($result);
@@ -66,8 +70,12 @@ function topics_link($result, $class = false)
   for($i = 0; $i < $n; ++$i)
   {
     extract(pg_fetch_array($result,$i,PGSQL_ASSOC));
-    if ($class) $name = format_class($name);
-    print("  <li><a href=\"$pagename?topic_id=$topic_id\">$name</a></li>\n");
+    if ($type == LINK_CLASS)
+      $name = format_class($name);
+    else if ($type == LINK_COURSE)
+      $name = format_course($name);
+    $style = $modified == 't' ? 'style="list-style-type: circle"' : 'style="list-style-type: disc"';
+    print("  <li $style><a href=\"$pagename?topic_id=$topic_id\">$name</a></li>\n");
   }
   print("</ul>\n");
 }
@@ -78,22 +86,23 @@ if (!$q || $q->state == SurveyEditor_done)
   
   wces_connect();
   
-  print("<p>Choose a survey to edit. Changes to surveys made in general categories propogate downward to surveys beneath them. For example, changes made to the base questions show up in the surveys for groups of classes and for individual classes. Changes made to a survey for a group of classes will show up in the surveys for all the classes in that group.</p>");
+  print("<p>Choose a survey to edit. Changes to surveys made in general categories propogate downward to surveys beneath them. For example, changes made to the base questions show up in the surveys for courses and for individual class sections. Changes made to a survey for a course will show up in surveys for that course's class sections.</p>");
   
-  $result = pg_query("SELECT topic_id, 'Base Questions' AS name FROM wces_topics WHERE category_id IS NULL AND class_id IS NULL", $wces, __FILE__, __LINE__);
-  topics_link($result);
+  $result = pg_query("SELECT topic_id, 'Base Questions' AS name, 't' AS modified FROM wces_topics WHERE course_id IS NULL AND class_id IS NULL", $wces, __FILE__, __LINE__);
+  topics_link($result, LINK_LITERAL);
   
-  print("<h5>Class Groupings</h5>\n");
+  print("<h5>Courses</h5>\n");
   
   $result = pg_query("
-    SELECT t.topic_id, c.name
+    SELECT t.topic_id, get_course(t.course_id) as name, 
+      topic_modified(t.topic_id) AS modified
     FROM wces_topics AS t
-    INNER JOIN survey_categories AS c ON c.survey_category_id = t.category_id
-    WHERE t.category_id IS NOT NULL AND t.class_id IS NULL
+    WHERE t.class_id IS NULL and t.course_id IS NOT NULL
+    ORDER BY name
   ", $wces, __FILE__, __LINE__);
-  topics_link($result);
+  topics_link($result, LINK_COURSE);
 
-  print("<h5>Individual Classes</h5>\n");
+  print("<h5>Class Sections</h5>\n");
 
   $result = pg_query("
     SELECT question_period_id, displayname, year, semester
@@ -103,13 +112,13 @@ if (!$q || $q->state == SurveyEditor_done)
   extract(pg_fetch_array($result,0,PGSQL_ASSOC));
   
   $result = pg_query("
-    SELECT t.topic_id, get_class(t.class_id) AS name
+    SELECT t.topic_id, get_class(t.class_id) AS name, topic_modified(t.topic_id) AS modified
     FROM wces_topics AS t
     INNER JOIN classes AS cl USING (class_id)
     WHERE t.category_id IS NOT NULL AND cl.year = $year AND cl.semester = $semester
     ORDER BY name
   ", $wces, __FILE__, __LINE__);
-  topics_link($result, true);
+  topics_link($result, LINK_CLASS);
 
 }
 
