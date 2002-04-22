5286f1f41cdfd3dedcdda4a0bbfed444cba3f0bf
Russell Yanofsky <russ@yanofsky.org>
2002-04-22 22:21:23 +0000

    student instructions

---
+++ www/wces/students/instructions.php
@@ -0,0 +1,37 @@
+<?
+
+require_once("wces/page.inc");
+require_once("wces/login.inc");
+
+login_protect(login_student);
+
+page_top("Student Instructions");
+
+?>
+<p>Complete All Course Evaluations/Be Eligible for to prizes. (see Poster or fallow up emails)</p>
+
+<p>To All Students Taking SEAS Courses (SEAS, College, Barnard, GSAS, etc.):</p>
+
+<p>URGENT! VERY IMPORTANT!</p>
+
+<p>We ask that you fill out on-line course evaluations for each of your Spring 2002 courses starting this Friday, April 19, 2002 through May 9, 2002, because:</p>
+
+<ol>
+<li>Present and future students trying to plan their courses of study will view your input.</li>
+<li>Your opinion is essential in decision-making process for teaching awards and faculty performance reviews. That translates into better academics for our school.</li>
+<li>You may WIN prizes (if you complete evaluations for all your courses by 5/09/2002.</li>
+</ol>
+ 
+<p>Where? At http://oracle.seas.columbia.edu/wces/</p>
+
+<p>(Click on "Students")</p.
+
+<p>Starting Friday, there will be posters placed throughout the MUDD Building to remind you of the details.</p>
+
+<p>DO NOT WAIT UNTIL THE LAST MINUTE! COMPLETE YOUR EVALUATIONS STARTING THIS FRIDAY!</p>
+
+<p>Any questions? Please email  WCES@columbia.edu.</p>
+
+<p>Thanks for your cooperation and watch for more of my famous emails!</p>
+
+<?  page_bottom();  ?>
