bd649caebfd00b7fc1f0a4c0f4bdd2c9ed5eb09f
Russell Yanofsky <russ@yanofsky.org>
2002-04-22 01:54:11 +0000

    fill up the new database with names and email addresses from ldap

--- www/wces/administrators/importldap.php
+++ www/wces/administrators/importldap.php
@@ -1,70 +1,81 @@
 <?
 
-require_once("wces/server.inc");
-
-require_once("wces/taskwindow.inc");
+require_once("wbes/server.inc");
 require_once("wces/page.inc");
 
-page_top("LDAP Import");
+page_top("LDAP Import", true);
 
-$db = wces_connect();
-$users = db_exec("
-  SELECT u.userid, u.cunix
+wces_connect();
+
+$users = pg_query("
+  SELECT u.user_id, u.uni, u.firstname, u.lastname, u.email
   FROM users AS u
-  LEFT JOIN ldapcache AS ld USING (userid)
-  WHERE u.email IS NULL", $db, __FILE__, __LINE__);
+  WHERE u.email IS NULL AND u.uni IS NOT NULL
+  ORDER BY u.uni
+", $wces, __FILE__, __LINE__);
 
 $ds=ldap_connect("ldap.columbia.edu");
 $r=ldap_bind($ds);
 
-$row = 0;
-taskwindow_start("Task Window");
-while($user = mysql_fetch_assoc($users))
+$n = pg_numrows($users);
+for($i = 0; $i < $n; ++$i)
 {
-  $cunix = $userid = "";
-  extract($user);
-  $sr=ldap_search($ds,"o=Columbia University,c=US", "uni=$cunix");  
+  $firstname = $lastname = "";
+  extract(pg_fetch_row($users, $i, PGSQL_ASSOC));
+  
+  bprint("[$i/$n] $uni user_id =$user_id ");
+
+  $sr=ldap_search($ds,"o=Columbia University,c=US", "uni=$uni");  
   $info = ldap_get_entries($ds, $sr);
-  if (isset($info[0]))
+  if ($info['count'] == 1)
   {
-    $cn = $info[0]["cn"][0];
+    $first = $info[0]["givenname"][0];
+    $last = $info[0]["sn"][0];
     $title = $info[0]["title"][0];
     $ou = $info[0]["ou"][0];
-    $email = $info[0]["mail"][0];
-    //db_addrow($db,"ldapcache", array("userid" => $userid, "cn" => $cn, "title" => $title, "ou" => $ou));
+    $email = isset($info[0]["mail"][0]) ? $info[0]["mail"][0] : "";
+
     if ($email)
     {
-      db_updatevalues($db,"users",array("userid"=>$userid),array("email" => $email));
-      taskwindow_cprint("<b>$cunix</b><br>\n - $email<br>\n");
-      // - $cn<br>\n - $title<br>\n - $ou<br>\n
+      bprint(", email = $email");
+      pg_query("UPDATE users SET email = '" . addslashes($email) . "' WHERE user_id = $user_id", $wces, __FILE__, __LINE__);
+    }
+    
+    if (!$firstname && !$lastname)
+    {
+      bprint(", lastname = '$last', firstname = '$first'");
+      pg_query("UPDATE users SET firstname = '" . addslashes($first) . "',
+        lastname = '" . addslashes($last) . "'
+        WHERE user_id = $user_id
+      ", $wces, __FILE__, __LINE__);
     }
   }
-  else
+  else if ($info['count'] != 0)
   {
-    taskwindow_cprint("<b>$cunix</b><br>\n");
     printarray($info,"info");
   }   
-  if ((++$row) % 5 == 0) taskwindow_flush();
+  bprint("<br>");
 }
-taskwindow_end();
 ldap_close($ds);
 
 if ($uni)
 {
-    print("<h4>Results</h4>\n");
-    print("<p><strong>" . $info["count"] . " results found</strong></p>");
-    foreach($info as $number => $result)
-    if (strcmp($number,"count") != 0)
-    {
-      print("<h5>" . $result["cn"][0] . "</h5>\n");
-      print("<ul>\n");
-      foreach($result as $itemname => $itemarray)
-      if(is_array($itemarray))
-        foreach($itemarray as $key => $value)
-        if (strcmp($key,"count") != 0)
-          print("  <li><code><b>$itemname</b> = $value</code></li>\n");
-      print("</ul>\n");
-    }
+  print("<h4>Results</h4>\n");
+  print("<p><strong>" . $info["count"] . " results found</strong></p>");
+  foreach($info as $number => $result)
+  if (strcmp($number,"count") != 0)
+  {
+    print("<h5>" . $result["cn"][0] . "</h5>\n");
+    print("<ul>\n");
+    foreach($result as $itemname => $itemarray)
+    if(is_array($itemarray))
+      foreach($itemarray as $key => $value)
+      if (strcmp($key,"count") != 0)
+        print("  <li><code><b>$itemname</b> = $value</code></li>\n");
+    print("</ul>\n");
   }
 }
+
+page_bottom(true);
+
 ?>
