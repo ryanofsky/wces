6c755f857d109efcbef7277bdfeed5c591991db4
Russell Yanofsky <russ@yanofsky.org>
2002-04-22 22:21:49 +0000

    instructions

---
+++ www/wces/professors/instructions.php
@@ -0,0 +1,34 @@
+<?
+
+require_once("wces/page.inc");
+require_once("wces/login.inc");
+
+login_protect(login_professor);
+
+page_top("Faculty Instructions");
+
+?>
+<P><SPAN>To all  Faculty:</SPAN></FONT></P>
+<P><SPAN>We are now preparing the WCES site  for Spring 2002 course evaluations. As a professor, there are two tasks  for you to complete prior to opening the site to  students:</SPAN></FONT></P>
+<P>
+1.From April 5th through April 18th please log on to the  web site (and review a brief user guide  for professors), and make sure that<SPAN> </SPAN>you and your course information are  correct.</SPAN></FONT></P>
+<P><SPAN>2. You have the capability to add questions to  the SEAS course<SPAN> </SPAN>evaluation survey. You may have some questions  you would like your<o:p> </o:p><SPAN> </SPAN>students to address for your specific  course(s). Please make any additions during this time period.  After April 18th, you will no longer be able to make  changes.</SPAN></FONT></P>
+<P><SPAN>If you want to modify your survey  for a specific course, follow these steps: </SPAN></FONT></P>
+<P><SPAN>1) Go to  http://www.esurveys.columbia.edu/wces/</SPAN></FONT></P>
+<P><SPAN>2) Type in your username and  password. This will take you to a screen that lists all the options currently  available to you as a professor.</SPAN></FONT></P>
+<P><SPAN>3) The first option says: "Upcoming  Surveys - Edit or preview the questions your students will see in  the upcoming question period." Click<o:p> </o:p>this  one.</SPAN></FONT></P>
+<P><SPAN><o:p> </o:p>4) The next screen lists the classes  that you are teaching this semester. Clicking on a class goes  directly to the Survey Builder page. </SPAN></FONT></P>
+<P><SPAN>5) The survey builder page shows the  survey for their class. The 10 core questions are pre-loaded. You can  insert multiple choice and text response questions below  these.</SPAN></FONT></P>
+<P><SPAN>6) After you have inserted  questions, clicking the "Save" button will take you back to the class listing  in step 4.</SPAN></FONT></P>
+<P><SPAN>There is no limit to the number you  can add, however too many questions can become burdensome to the  student. The 10 basic SEAS questions are standard.</SPAN></FONT></P>
+<P><SPAN>(ABET items for SEAS  Faculty)</SPAN></FONT></P>
+<P><SPAN>Additionally, there are several ABET  related questions that you can add to the survey by  checking the ones that are appropriate for your course. Although customizing  your survey is optional, we highly recommend that you add questions based on your course objectives,<o:p> </o:p>especially if they relate to any of  the eleven ABET (a-k) learning outcomes. If you want to collect  information on the core questions only, you do not need to take this  step.</SPAN></FONT></P>
+<P><SPAN>Starting April 19th, the website will be open  for student input. It is<SPAN> </SPAN>important that you frequently remind your  students of the importance of<o:p> </o:p>these evaluations. Please plan to mention it  during each class throughout the evaluation  period.</SPAN></FONT></P>
+<P><SPAN><B><SPAN>Spring 2001 EVALUATION  SCHEDULE:</SPAN></B></SPAN></FONT></P>
+<P><B><SPAN>April  5 - 18, 2002 --- faculty log on and add short questions to the evaluation  survey.</SPAN></B></P>
+<P><B><SPAN>April  19 - May 9, 2002 --- students enter their course  evaluations.</SPAN></B></P>
+<P><B><SPAN>June  3, 2002 --- Your results for each course will be sent to you via email.</SPAN></B></P>
+<P><SPAN>Many thanks</SPAN></P>
+<P><SPAN>Morton B.  Friedman<br> Jack  McGourty<br> Jackie  Pavlik</SPAN></FONT></P>
+
+<?  page_bottom();  ?>
