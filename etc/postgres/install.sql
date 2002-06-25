710c893ddc066989abebbc51b7e621ec332b80b6
Russell Yanofsky <russ@yanofsky.org>
2002-06-25 16:49:19 +0000

    added paths for oracle

--- etc/postgres/install.sql
+++ etc/postgres/install.sql
@@ -5,3 +5,12 @@ LANGUAGE 'c' WITH (iscachable);
 CREATE FUNCTION int_mdistf(integer[], integer[]) RETURNS integer[] AS
   '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres/distribution.dll', 'int_mdistf'
 LANGUAGE 'c' WITH (iscachable);
+
+CREATE FUNCTION int_distf(integer[], integer) RETURNS integer[] AS
+  '/home/httpd/etc/postgres/distribution.so', 'int_distf'
+LANGUAGE 'c' WITH (iscachable);
+
+CREATE FUNCTION int_mdistf(integer[], integer[]) RETURNS integer[] AS
+  '/home/httpd/etc/postgres/distribution.so', 'int_mdistf'
+LANGUAGE 'c' WITH (iscachable);
+
