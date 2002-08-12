da53ce1df488550632faa04f1e70b1fe2f19dc3e
Russell Yanofsky <russ@yanofsky.org>
2002-08-12 05:16:53 +0000

    updated paths

--- etc/postgres2/install.sql
+++ etc/postgres2/install.sql
@@ -24,3 +24,11 @@ CREATE AGGREGATE scat_dist (
     sfunc = int_scat_distf
 );
 
+CREATE FUNCTION int_scat_distf(integer[], integer) RETURNS integer[] AS
+  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres2/scattered_distribution.dll', 'int_scat_distf'
+LANGUAGE 'c' WITH (iscachable);
+
+CREATE FUNCTION int_scat_modef(integer[]) RETURNS integer AS
+  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres2/scattered_distribution.dll', 'int_scat_modef'
+LANGUAGE 'c' WITH (iscachable);
+
