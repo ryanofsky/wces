df2ebc192a8c4c7582ea4dbd148672fe8a4ae44b
Russell Yanofsky <russ@yanofsky.org>
2002-08-12 05:16:02 +0000

    updated with new paths

--- etc/postgres/install.sql
+++ etc/postgres/install.sql
@@ -6,6 +6,9 @@ CREATE FUNCTION int_mdistf(integer[], integer[]) RETURNS integer[] AS
   '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres/distribution.dll', 'int_mdistf'
 LANGUAGE 'c' WITH (iscachable);
 
+DROP FUNCTION int_distf(integer[], integer);
+DROP FUNCTION int_mdistf(integer[], integer[]);
+
 CREATE FUNCTION int_distf(integer[], integer) RETURNS integer[] AS
   '/home/httpd/etc/postgres/distribution.so', 'int_distf'
 LANGUAGE 'c' WITH (iscachable);
