25065869f00ac34a5a10bf501238022b0dbdf2b1
Russell Yanofsky <russ@yanofsky.org>
2002-11-27 08:07:20 +0000

    older than HEAD

--- www/wces/professors/new_admin.php
+++ www/wces/professors/new_admin.php
@@ -3,85 +3,58 @@
 require_once("wces/login.inc");
 require_once("wces/prespond.inc");
 
-define('ProfResponseList_choose', 1);
+$user_id = login_getuserid();
+$profname = login_getname();
 
-class ProfResponseList extends StatefullWidget
+function listclasses()
 {
-  function ProfResponseList($name, &$parent)
-  {
-    $this->StatefullWidget($name, $parent);
-    $this->event->shortName(ProfResponseList_choose, array('course_id', 'user_id'));
-  }
+  global $user_id, $profname, $wces, $select_classes;
 
-  function & handleEvent($event, $param, $isNew)
+  wces_connect();
+
+  $result = pg_go("
+    SELECT p.user_id, p.course_id, get_course(p.course_id) AS course_info, u.firstname, u.lastname
+    FROM presps AS p
+    INNER JOIN users AS u USING (user_id)
+  ", $wces, __FILE__, __LINE__);
+
+  $classes = new pg_wrapper($result);
+
+  print("<p>Choose a professor response.</p>\n");
+  print("<ul>\n");
+  if ($classes->rows == 0)
   {
-    global $base_branch_id, $user_id, $factories;
-    if ($event == ProfResponseList_choose)
+    print("  <li><i>No courses found</i></li>");
+  }
+  else
+  {
+    while($classes->row)
     {
-      $course_id = $param[0];
-      $user_id = $param[1];
-      ProfResponse::DumpScript();
-      $this->response =& new ProfResponse($course_id, $user_id, true, 'response', $this);
-      $this->loadChild($this->response, $isNew);
-      return $this->response;
+      extract($classes->row);
+      print("  <li><a href=\"new_admin.php?course_id=$course_id&user_id=$user_id\">" 
+        . format_ccourse($course_info, "<i>%c</i> %n")
+        . "</a>"
+        . " from $firstname $lastname</li>\n");
+      $classes->advance();
     }
   }
-  
-  function printModalChildren()
-  {
-    print("<p><a href=new_admin.php>Back</a></p>\n");
-    StatefullWidget::printModalChildren(); 
-  } 
-    
-  function printVisible()
-  { 
-    global $user_id, $profname, $wces, $select_classes, $ASID;
-
-    $result = pg_go("
-      SELECT p.user_id, p.course_id, get_course(p.course_id) AS course_info, u.firstname, u.lastname
-      FROM presps AS p
-      INNER JOIN users AS u USING (user_id)
-    ", $wces, __FILE__, __LINE__);
-  
-    $classes = new pg_wrapper($result);
-  
-    if (isset($this->response->message))
-      print($this->response->message);
-
-    print("<p>Choose a professor response.</p>\n");
-    print("<ul>\n");
-    if ($classes->rows == 0)
-    {
-      print("  <li><i>No courses found</i></li>");
-    }
-    else
-    {
-      while($classes->row)
-      {
-        extract($classes->row);
-        print('  <li><a href="'
-          . $this->event->getUrl(ProfResponseList_choose, array($course_id, $user_id))
-          . "$ASID\">" . format_ccourse($course_info, "<i>%c</i> %n")
-          . "</a>"
-          . " from $firstname $lastname</li>\n");
-        $classes->advance();
-      }
-    }
-    print("</ul>\n");
-  }
+  print("</ul>\n");
 }
 
-$f =& new Form();
-$l =& new ProfResponseList('menu', $f);
-$f->loadState();
+param($course_id);
+param($user_id);
+param($save);
+$course_id = (int) $course_id;
+$user_id = (int) $user_id;
 
-page_top("Professor Responses");
-
-print("<form method=post action=\"$f->pageName\">\n$ISID\n");
-$f->display();
-$l->display();
-print("</form>\n");
-
+page_top("Survey Results");
+if ($course_id && $user_id)
+{
+    print("<p><a href=new_admin.php>Back</a></p>\n");
+    showResults($user_id, $course_id, false);
+}
+else
+  listclasses();
 page_bottom();
 
 ?>
--- www/wces/professors/new_prof.php
+++ www/wces/professors/new_prof.php
@@ -8,82 +8,63 @@ login_protect(login_professor);
 $user_id = login_getuserid();
 $profname = login_getname();
 
-define('ProfResponseMenu_choose', 1);
-
-class ProfResponseMenu extends StatefullWidget
+function listclasses()
 {
-  function ProfResponseMenu($name, &$parent)
-  {
-    $this->StatefullWidget($name, $parent);
-    $this->event->shortName(ProfResponseMenu_choose, "course_id");
-  }
+  global $user_id, $profname, $wces, $select_classes;
 
-  function & handleEvent($event, $param, $isNew)
+  $uid = (int) login_getuserid();
+  wces_connect();
+
+  $result = pg_go("
+    SELECT cl.course_id, get_course(cl.course_id) AS course_info
+    FROM enrollments AS e
+    INNER JOIN ($select_classes) AS l USING (class_id)
+    INNER JOIN classes AS cl USING (class_id)
+    WHERE e.user_id = $uid AND e.status = 3
+    GROUP BY cl.course_id
+    ORDER BY course_info
+  ", $wces, __FILE__, __LINE__);
+
+  $classes = new pg_wrapper($result);
+
+  print("<p>Choose a course to review its results.</p>\n");
+  print("<ul>\n");
+  if ($classes->rows == 0)
   {
-    global $base_branch_id, $user_id, $factories;
-    if ($event == ProfResponseMenu_choose)
+    print("  <li><i>No courses found</i></li>");
+  }
+  else
+  {
+    while($classes->row)
     {
-      $user_id = login_getuserid();
-      ProfResponse::DumpScript();
-      $this->response =& new ProfResponse($param, $user_id, false, 'response', $this);
-      $this->loadChild($this->response, $isNew);
-      return $this->response;
+      extract($classes->row);
+      print("  <li><a href=\"new_prof.php?course_id=$course_id\">" . format_ccourse($course_info, "<i>%c</i> %n") . "</a></li>\n");
+      $classes->advance();
     }
   }
-  
-  function printVisible()
-  {
-    global $user_id, $profname, $wces, $select_classes, $ASID;
-  
-    if (isset($this->response->message))
-      print($this->response->message);
-    
-    $uid = (int)login_getuserid();
-    wces_connect();
-  
-    $result = pg_go("
-      SELECT cl.course_id, get_course(cl.course_id) AS course_info
-      FROM enrollments AS e
-      INNER JOIN ($select_classes) AS l USING (class_id)
-      INNER JOIN classes AS cl USING (class_id)
-      WHERE e.user_id = $uid AND e.status = 3
-      GROUP BY cl.course_id
-      ORDER BY course_info
-    ", $wces, __FILE__, __LINE__);
-  
-    $classes = new pg_wrapper($result);
-  
-    print("<p>Choose a course to review its results.</p>\n");
-    print("<ul>\n");
-    if ($classes->rows == 0)
-      print("  <li><i>No courses found</i></li>");
-    else
-    {
-      while($classes->row)
-      {
-        extract($classes->row);
-        print('  <li><a href="'
-          . $this->event->getUrl(ProfResponseMenu_choose, $course_id)
-          ."$ASID\">" . format_ccourse($course_info, "<i>%c</i> %n")
-          . "</a></li>\n");
-        $classes->advance();
-      }
-    }
-    print("</ul>\n");    
-  }
+  print("</ul>\n");
 }
 
-$f =& new Form();
-$l =& new ProfResponseMenu('menu', $f);
-$f->loadState();
+param($course_id);
+param($save);
+$course_id = (int) $course_id;
 
 page_top("Survey Results");
-
-print("<form method=post action=\"$f->pageName\">\n$ISID\n");
-$f->display();
-$l->display();
-print("</form>\n");
-
+if ($course_id)
+{
+  if ($save)
+  {
+    saveResults($user_id, $course_id);  
+    listclasses();
+  }
+  else
+  {
+    print("<p><a href=new_prof.php>Back</a></p>\n");
+    showResults($user_id, $course_id, true);
+  }
+}
+else
+  listclasses();
 page_bottom();
 
 ?>
