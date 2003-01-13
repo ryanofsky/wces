836e8f44b6c2c0d9d870c6105ca95d6bd1a62ae4
Russell Yanofsky <russ@yanofsky.org>
2003-01-13 19:22:50 +0000

    automatically filled in email address

--- www/wces/about/feedback.php
+++ www/wces/about/feedback.php
@@ -1,6 +1,8 @@
 <?
 require_once("wces/page.inc");
 require_once("wbes/general.inc");
+require_once("wces/login.inc");
+
 page_top("Feedback");
 
 param($comments);
@@ -11,8 +13,18 @@ if (!$comments)
 <p>Thank you for visiting the Web-based Course Evaluation System (WCES). In order for us to further improve this site, we would appreciate your feedback.</p>
 <form name=mail method=post action="feedback.php">
 <?=$ISID?>
+<?
+  $email = '';
+  if ($user_id = login_getuserid())
+  {
+    wces_connect();
+    $result = pg_go("SELECT email FROM users WHERE user_id = $user_id", $wces, __FILE__, __LINE__);
+    if (pg_numrows($result) == 1)
+      $email = htmlspecialchars(pg_result($result, 0, 0));
+  }
+?>
 <table>
-<tr><td valign=top>Email (optional) </td><td><input type=text name=email size=30></td></tr>
+<tr><td valign=top>Email (optional) </td><td><input type=text name=email value="<?=$email?>" size=30></td></tr>
 <tr><td valign=top>Comments </td><td><textarea name=comments rows=7 cols=40></textarea></td></tr>
 <tr><td valign=top>&nbsp;</td><td><input type=submit name=submit value="Submit"></td></tr>
 </table>
