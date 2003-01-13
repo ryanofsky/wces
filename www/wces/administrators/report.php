e4341c34b2c14ca5eb7e68df6bdddfec62ab19db
Russell Yanofsky <russ@yanofsky.org>
2003-01-13 19:27:30 +0000

    allowed caching of reporting wizard pages

--- www/wces/administrators/report.php
+++ www/wces/administrators/report.php
@@ -1,12 +1,20 @@
 <?
- require_once("wces/page.inc");
- require_once("wces/login.inc");
- require_once("wces/report_widget.inc");
- login_protect(login_administrator | login_deptadmin);
- 
- $report = new Report("report","wiz",WIDGET_POST);
- $report->loadvalues();
- page_top("Administrative Report",$report->hidemenus);
+  require_once("wces/page.inc");
+  require_once("wces/login.inc");
+  require_once("wces/report_widget.inc");
+  
+  login_protect(login_administrator | login_deptadmin);
+  
+  $seconds = 3600;
+  session_cache_limiter('public');
+  header('Cache-Control: public');
+  header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $seconds) . ' GMT');
+  header('Last-Modified: ' . gmdate('D, d M Y H:i:s', getlastmod()) . ' GMT');
+  
+  $report = new Report("report","wiz",WIDGET_POST);
+  $report->loadvalues();
+  
+  page_top("Administrative Report",$report->hidemenus);
 ?>
 
 <form name="wiz" method="post">
