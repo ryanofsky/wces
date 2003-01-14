c1e96f3597b5794e75a97de745121c0fc56709f3
Russell Yanofsky <russ@yanofsky.org>
2003-01-14 05:11:47 +0000

    enable caching of reporting wizard pages

--- www/wces/administrators/report.php
+++ www/wces/administrators/report.php
@@ -1,20 +1,29 @@
 <?
- require_once("wces/page.inc");
- require_once("wces/login.inc");
- require_once("wces/report_widget.inc");
- login_protect(login_administrator | login_deptadmin);
- 
- $f =& new Form('wiz');
- $report =& new Report('report', $f);
- //DebugBreak();
- $f->loadState();
- page_top("Administrative Report",$report->hideMenus);
- 
- print("<form name={$f->formName} action=\"{$f->pageName}\" method=post>\n");
- print($ISID);
- $f->display();
- $report->display();
- print("</form>");
 
- page_bottom($report->hideMenus);
+  require_once("wces/page.inc");
+  require_once("wces/login.inc");
+  require_once("wces/report_widget.inc");
+  login_protect(login_administrator | login_deptadmin);
+ 
+  $f =& new Form('wiz');
+  $report =& new Report('report', $f);
+  //DebugBreak();
+  $f->loadState();
+
+  $seconds = 3600;
+  session_cache_limiter('public');
+  header('Cache-Control: public');
+  header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $seconds) . ' GMT');
+  header('Last-Modified: ' . gmdate('D, d M Y H:i:s', getlastmod()) . ' GMT');
+
+  page_top("Administrative Report",$report->hideMenus);
+ 
+  print("<form name={$f->formName} action=\"{$f->pageName}\" method=post>\n");
+  print($ISID);
+  $f->display();
+  $report->display();
+  print("</form>");
+
+  page_bottom($report->hideMenus);
+
 ?>
