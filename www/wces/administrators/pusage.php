852cd464ed9aef142d894cd60c3a7ca354d6a574
Russell Yanofsky <russ@yanofsky.org>
2003-01-19 23:07:22 +0000

    rewritten with widgets

--- www/wces/administrators/pusage.php
+++ www/wces/administrators/pusage.php
@@ -4,165 +4,26 @@ require_once("wbes/server.inc");
 require_once("wces/login.inc");
 require_once("wces/page.inc");
 require_once("wces/usage.inc");
+
 login_protect(login_administrator);
 
-param('question_period_id');
-param('category_id');
-
-wces_connect();
-
-// generate lists of options
-$questionPeriodChoices = usage_sql_array("
-  SELECT q.question_period_id, q.displayname
-  FROM semester_question_periods AS q
-  WHERE EXISTS (SELECT * FROM wces_topics AS t WHERE t.question_period_id = q.question_period_id)
-  ORDER BY begindate
-", $wces, __FILE__, __LINE__);
-
-$categoryChoices = usage_sql_array("
-  SELECT 0 AS id, 'None'
-  UNION
-  SELECT c.survey_category_id, c.name
-  FROM survey_categories AS c
-  ORDER BY id
-", $wces, __FILE__, __LINE__);
-
-// figure out current values
-if (!isset($category_id) || !isset($categoryChoices[$category_id])) $category_id = 0;
-if (isset($question_period_id) && isset($questionPeriodChoices[$question_period_id]))
-  set_question_period($question_period_id);
-else
-  $question_period_id = get_question_period();
-
-$currentValues = array('question_period_id' => $question_period_id,
-  'category_id' => $category_id);
+$f =& new Form(null, $PHP_SELF);
+$u =& new ProfessorUsage('usage', $f);
+$f->loadState();
 
 // get information about selected question period
 $result = pg_go("
-  SELECT question_period_id, displayname, year, semester
+  SELECT displayname
   FROM semester_question_periods
-  WHERE question_period_id = $question_period_id
+  WHERE question_period_id = {$u->options->question_period_id}
 ", $wces, __FILE__, __LINE__);
 
 extract(pg_fetch_array($result,0,PGSQL_ASSOC));
 
 page_top("Professor Usage Data for $displayname");
 
-print("<p>Question Period: "); 
-usage_menu($questionPeriodChoices, 'question_period_id', $currentValues);
-print("<br>\n");
+$u->display();
 
-print("Filtering: "); 
-usage_menu($categoryChoices, 'category_id', $currentValues);
-print("<br>\n");
-
-$cat = $category_id ? " AND t.category_id = $category_id" : '';
-pg_go("
-  CREATE TEMPORARY TABLE tcl AS
-  SELECT t.class_id, cl.students, u.user_id, u.lastname, u.uni, 
-    u.firstname, u.lastlogin, get_class(t.class_id) AS class_info,
-    CASE WHEN specialization_modified(t.item_id, t.specialization_id)
-    THEN 1 ELSE 0 END AS customized,
-    CASE WHEN (select timestamp 'now'  - interval '30 days') < u.lastlogin
-    THEN 1 ELSE 0 END AS loggedin
-  FROM wces_topics AS t
-  INNER JOIN classes AS cl USING (class_id)
-  INNER JOIN enrollments AS e ON e.class_id = cl.class_id AND e.status = 3
-  INNER JOIN users AS u USING (user_id)
-  WHERE t.question_period_id = $question_period_id$cat;  
-",$wces,__FILE__, __LINE__);
-
-pg_go("
-  CREATE TEMPORARY TABLE tst AS
-  SELECT class_id, CASE WHEN customized <> 0 THEN 0 WHEN sum(loggedin) > 0 THEN 1 
-    ELSE 2 END AS status 
-  FROM tcl
-  GROUP BY class_id, customized;
-",$wces,__FILE__, __LINE__);
-
-pg_go("
-  CREATE TEMPORARY TABLE tpr AS
-  SELECT user_id, CASE WHEN SUM(customized) > 0 THEN 0 WHEN loggedin <> 0 THEN 1
-    ELSE 2 END AS status
-  FROM tcl
-  GROUP BY user_id, loggedin;
-",$wces,__FILE__, __LINE__);
-
-$profcount = pg_result(pg_go("SELECT COUNT(*) FROM tpr",$wces,__FILE__,__LINE__), 0, 0);
-$profcustomized = pg_result(pg_go("SELECT COUNT(*) FROM tpr WHERE status = 0", $wces, __FILE__, __LINE__), 0, 0);
-$profloggedin = pg_result(pg_go("SELECT COUNT(*) FROM tpr WHERE status = 1", $wces, __FILE__, __LINE__), 0, 0);
-
-?>
-
-<h3>Aggregate Usage</h3>
-
-Number of professors: <b><?=(int)$profcount?></b><br>
-Number of professors who logged in during the customization period: <b><?=(int)$profloggedin?></b><br>
-Number of professors who created custom surveys: <b><?=(int)$profcustomized?></b><br>
-<img src="<?=$wces_path?>media/graphs/pusagegraph.php?neverloggedin=<?=(int)$profcount-(int)$profloggedin?>&custom=<?=(int)$profcustomized?>&nocustom=<?=(int)$profloggedin-(int)$profcustomized?>" width=200 height=200><img src="<?=$wces_path?>media/graphs/pusagelegend.gif" width=133 height=49><br>
-<p>&nbsp;</p>
-
-<h3>Individual Class Usage</h3>
-
-<?
-
-$result = pg_go("
-  SELECT tst.status, tcl.class_id, tcl.students, tcl.lastname, tcl.firstname, tcl.class_info, tcl.user_id,
-    to_char(tcl.lastlogin,'YYYY-MM-DD') AS lastlogin
-  FROM tcl INNER JOIN tst USING (class_id)
-  ORDER BY tst.status, tcl.class_info, tcl.class_id 
-", $wces, __FILE__, __LINE__);
-
-pg_go("DROP TABLE tcl; DROP TABLE tst; DROP TABLE tpr", $wces, __FILE__, __LINE__);
-
-$r =& new pg_segmented_wrapper($result, array('status', 'class_id'));
-
-print("<table cellspacing=1 cellpadding=1 border=1>\n"); 
-print("<tr><td><b>Code</b></td><td><b>Name</b></td><td><b>Size</b></td><td><b>Professor</b></td><td><b>Last Login</b></td></tr>\n");
-while ($r->row)
-{
-  if ($r->split[0])
-  {
-    print("<tr><td colspan=5><strong><font color=\"#0030E7\">");
-    if ($r->row['status'] == 0)
-      print("Classes with custom surveys:");
-    else if ($r->row['status'] == 1)
-      print("Classes with professors who have recently logged in:");
-    else
-      print("Classes without professors who have recently logged in:");    
-    print("</font></strong></td></tr>\n");
-  }
-  
-  if ($r->split[1])
-  {
-    $rows = 0;
-    $pr = '';
-  }
-  else 
-    $pr .= '<tr>';
-  
-  ++$rows;
-  
-  extract($r->row);
-  $pr .= "<td><a href=\"{$wces_path}administrators?user_id=$user_id\">$firstname $lastname</a></td>";
-  $pr .= $lastlogin ? "<td>$lastlogin</td>" : "<td><i>never</i></td>";
-  $pr .= "</tr>\n";
-  
-  $r->advance();
-
-  if ($r->split[1])
-  {
-    $rows = $rows == 1 ? '' : " rowspan=$rows";
-    print("<tr><td$rows>");
-    print(format_class($class_info, "%c %s"));
-    print("</td><td$rows>");
-    print(format_class($class_info, "%n"));
-    print("</td><td$rows>$students</td>");
-    print($pr);
-  }
-}
-
-print("</table>\n");
 
 page_bottom();
 ?>
--- www/wces/administrators/susage.php
+++ www/wces/administrators/susage.php
@@ -1,215 +1,28 @@
 <?
+
 require_once("wbes/server.inc");
 require_once("wces/login.inc");
 require_once("wces/page.inc");
+require_once("wces/usage.inc");
 
 login_protect(login_administrator);
 
-param('category_id');
-param('sort');
-param('question_period_id');
-
-wces_connect();
-
-// generate lists of options
-$questionPeriodChoices = usage_sql_array("
-  SELECT q.question_period_id, q.displayname
-  FROM semester_question_periods AS q
-  WHERE EXISTS (SELECT * FROM wces_topics AS t WHERE t.question_period_id = q.question_period_id)
-  ORDER BY begindate
-", $wces, __FILE__, __LINE__);
-
-$categoryChoices = usage_sql_array("
-  SELECT 0 AS id, 'None'
-  UNION
-  SELECT c.survey_category_id, c.name
-  FROM survey_categories AS c
-  ORDER BY id
-", $wces, __FILE__, __LINE__);
-
-$sortChoices = array("Course Code", "Number of surveys left", "Number of surveys completed"); 
-$sortSql = array("cl", "students - responses DESC", "responses DESC");
-
-// figure out current values
-if (!isset($sort) || !isset($sortChoices[$sort])) $sort = 0;
-if (!isset($category_id) || !isset($categoryChoices[$category_id])) $category_id = 0;
-if (isset($question_period_id) && isset($questionPeriodChoices[$question_period_id]))
-  set_question_period($question_period_id);
-else
-  $question_period_id = get_question_period();
-
-$currentValues = array('question_period_id' => $question_period_id,
-  'category_id' => $category_id, 'sort' => $sort);
+$f =& new Form(null, $PHP_SELF);
+$u =& new StudentUsage('usage', $f);
+$f->loadState();
 
 // get information about selected question period
 $result = pg_go("
   SELECT question_period_id, displayname, year, semester
   FROM semester_question_periods
-  WHERE question_period_id = $question_period_id
+  WHERE question_period_id = {$u->options->question_period_id}
 ", $wces, __FILE__, __LINE__);
 
 extract(pg_fetch_array($result,0,PGSQL_ASSOC));
 
 page_top("Student Usage Data for $displayname");
 
-
-print("<p>Question Period: "); 
-usage_menu($questionPeriodChoices, 'question_period_id', $currentValues);
-print("<br>\n");
-
-print("Filtering: "); 
-usage_menu($categoryChoices, 'category_id', $currentValues);
-print("<br>\n");
-
-print("Sorting: "); 
-usage_menu($sortChoices, 'sort', $currentValues);
-print("</p>\n");
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-$times = array();
-$times["begin"] = microtime();
-
-$cat = $category_id ? "AND t.category_id = $category_id" : "";
-
-//todo: use user_id instead of response_id
-pg_go("
-  CREATE TEMPORARY TABLE surveycounts AS
-  SELECT t.class_id, COUNT(DISTINCT response_id)::INTEGER AS responses
-  FROM wces_topics AS t
-  INNER JOIN classes AS cl USING (class_id)
-  LEFT JOIN survey_responses AS s USING (topic_id)
-  WHERE t.question_period_id = $question_period_id $cat
-  GROUP BY t.class_id
-",$wces,__FILE__, __LINE__);
-
-$times["count_responses"] = microtime();
-
-pg_go("
-  CREATE TEMPORARY TABLE surveyclasses AS
-  SELECT cc.class_id, COALESCE(cl.students,0) AS students, COALESCE(cc.responses,0) AS responses
-  FROM surveycounts AS cc
-  INNER JOIN classes AS cl USING (class_id)
-",$wces,__FILE__, __LINE__);
-
-$times["count_enrollments"] = microtime();
-
-$y = pg_go("SELECT SUM(students) as students, SUM(responses) as responses FROM surveyclasses",$wces,__FILE__, __LINE__);
-
-$times["sum_counts"] = microtime();
-
-extract(pg_fetch_array($y,0,PGSQL_ASSOC));
-
-///////////////////////////////////////////////////////////////////////////////
-
-?>
-
-<h3>Aggregate Student Usage</h3>
-
-Total number of surveys: <b><?=$students?></b><br>
-Number of surveys completed: <b><?=$responses?></b><br>
-
-<img src="<?=$wces_path?>media/graphs/susagegraph.php?blank=<?=$students-$responses?>&filled=<?=$responses?>" width=200 height=200><img src="<?=$wces_path?>media/graphs/susagelegend.gif" width=147 height=31><br>
-
-<h3>Individual Class Usage</h3>
-<p><font size="-1">Sorted by number of surveys that haven't been filled out</font></p>
-
-<?
-
-$classes = pg_go("
-  SELECT CASE WHEN students < responses THEN responses ELSE students END AS students, responses, get_class(class_id) AS cl, get_profs(class_id) AS p
-  FROM surveyclasses
-  ORDER BY $sortSql[$sort]
-",$wces, __FILE__, __LINE__);
-
-$times["get_names"] = microtime();
-
-$n = pg_numrows($classes);
-
-?>
-<table border=1>
-<tr>
-  <td align=center valign=bottom><img src="<?=$wces_path?>media/report/surveys_completed.gif" width=16 height=151 border=0 alt="Surveys Completed"></td>
-  <td align=center valign=bottom><img src="<?=$wces_path?>media/report/surveys_left.gif" width=16 height=94 border=0 alt="Surveys Left"></td>
-  <td align=center valign=bottom><img src="<?=$wces_path?>media/report/total_surveys.gif" width=16 height=105 border=0 alt="Total Surveys"></td>  
-  <td align=center valign=bottom>Class Information</td>
-</tr>
-<?
-
-for($i=0; $i<$n; ++$i)
-{
-  extract(pg_fetch_array($classes,$i,PGSQL_ASSOC));
-  print("<tr>\n  <td>$responses</td>\n  <td>" . ($students - $responses)  . "</td>\n  <td>$students</td>\n");
-  $classinfo = format_class($cl, "%c %n Section %s", true) . format_profs($p, true, "<br>Professor ");
-  print("  <td>$classinfo</td>\n");  
-  print("</tr>\n");
-}
-
-?>
-
-</table>
-<?
-
-
-
-flush();
-
-///////////////////////////////////////////////////////////////////////////////
-
-/*
-
-$cat = $survey_category_id ? "AND t.category_id = $survey_category_id" : "";
-
-$students = pg_go("
-  SELECT i.user_id, u.uni, CASE WHEN i.responses = 0 THEN 0 WHEN i.classes <= i.responses THEN 2 ELSE 1 END AS level
-  FROM
-    (SELECT e.user_id, COUNT (DISTINCT t.topic_id) AS classes, COUNT(DISTINCT s.topic_id) AS responses
-    FROM question_periods_topics AS qt
-    INNER JOIN wces_topics AS t USING (topic_id)
-    INNER JOIN enrollments AS e ON e.class_id = t.class_id AND e.status = 1
-    LEFT JOIN survey_responses AS s ON s.user_id = e.user_id AND s.topic_id = t.topic_id AND s.question_period_id = qt.question_period_id
-    WHERE qt.question_period_id = $question_period_id $cat
-    GROUP BY e.user_id) AS i
-  INNER JOIN users AS u USING (user_id)
-  ORDER BY level DESC, random();
-", $wces, __FILE__, __LINE__);
-
-$times["get_individual_students"] = microtime();
-
-print("<h3>Individual Student Usage</h3>\n");
-
-$levels = array
-(
-  2 => "<h4>Students who completed all of their surveys</h4>\n<blockquote>",
-  1 => "\n</blockquote>\n<h4>Students who completed at least one of their surveys</h4>\n<blockquote>",
-  0 => "\n</blockquote>\n<h4>Students who completed at none of their surveys</h4>\n<blockquote>"
-);
-
-$oldlevel = "";
-
-$n = pg_numrows($students);
-for($i = 0; $i < $n; ++$i)
-{
-  extract(pg_fetch_row($students, $i, PGSQL_ASSOC));
-  if (!($level === $oldlevel))
-  {
-    print($levels[$level]);
-    $first = true;
-  }
-  if ($first) $first = false; else print(", ");
-  print("\n  <a href=\"${wces_path}administrators/info.php?user_id=$user_id$ASID\">$uni</a>");
-  $oldlevel = $level;
-}
-print("</blockquote>");
-
-$times["print_individual_students"] = microtime();
-
-*/
-
-printtimes($times);
+$u->display();
 
 page_bottom();
 ?>
