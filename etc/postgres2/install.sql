f891927adfc8e97f41bcfa23a04574b091f9da73
Russell Yanofsky <russ@yanofsky.org>
2003-01-19 22:35:42 +0000

    removed old paths

--- etc/postgres2/install.sql
+++ etc/postgres2/install.sql
@@ -4,11 +4,11 @@ DROP FUNCTION int_scat_distf(integer[], integer);
 DROP FUNCTION int_scat_modef(integer[]);
 
 CREATE FUNCTION int_scat_distf(integer[], integer) RETURNS integer[] AS
-  '/home/httpd/etc/postgres2/scattered_distribution.so', 'int_scat_distf'
+  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres2/scattered_distribution.dll', 'int_scat_distf'
 LANGUAGE 'c' WITH (iscachable);
 
 CREATE FUNCTION int_scat_modef(integer[]) RETURNS integer AS
-  '/home/httpd/etc/postgres2/scattered_distribution.so', 'int_scat_modef'
+  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres2/scattered_distribution.dll', 'int_scat_modef'
 LANGUAGE 'c' WITH (iscachable);
 
 CREATE AGGREGATE mode (
@@ -23,12 +23,4 @@ CREATE AGGREGATE scat_dist (
     stype = INTEGER[],
     sfunc = int_scat_distf
 );
-
-CREATE FUNCTION int_scat_distf(integer[], integer) RETURNS integer[] AS
-  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres2/scattered_distribution.dll', 'int_scat_distf'
-LANGUAGE 'c' WITH (iscachable);
-
-CREATE FUNCTION int_scat_modef(integer[]) RETURNS integer AS
-  '/cygdrive/l/server/shares/russ.esurveys.hn.org/etc/postgres2/scattered_distribution.dll', 'int_scat_modef'
-LANGUAGE 'c' WITH (iscachable);
 
