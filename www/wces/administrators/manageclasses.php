2dbba49e9cf8e3e994a4a8c730922512d71a5392
raj <>
2003-06-16 17:43:26 +0000

    Created by Raj...

---
+++ www/wces/administrators/manageclasses.php
@@ -0,0 +1,12 @@
+<?
+
+require_once("wbes/server.inc");
+require_once("wces/login.inc");
+require_once("wces/page.inc");
+require_once("wces/usage.inc");
+
+LoginProtect(LOGIN_ADMIN);
+
+print "Login OK";
+
+?>
